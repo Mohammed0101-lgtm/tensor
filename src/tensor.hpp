df9b13d419f91d5e82895a7163ef5dba14d74ff6
mohamed <mohamed.bayt.alkhalij@gmail.com>
2024-11-30 13:00:15 +0100

    stuff

---
+++ src/tensor.hpp
@@ -0,0 +1,12 @@
+#pragma once 
+
+
+#include "tensorbase.hpp"
+#include "arithmetic.hpp"
+#include "bit.hpp"
+#include "compare.hpp"
+#include "data.hpp"
+#include "linear.hpp"
+#include "logical.hpp"
+#include "types.hpp"
+
