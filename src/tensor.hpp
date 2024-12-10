f30e2dcc864ccd5a8cc626d222104e304f9499e9
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2024-12-10 19:44:22 +0100

    stuff

--- src/tensor.hpp
+++ src/tensor.hpp
@@ -1,12 +1,11 @@
-#pragma once 
+#pragma once
 
 
-#include "tensorbase.hpp"
 #include "arithmetic.hpp"
 #include "bit.hpp"
 #include "compare.hpp"
 #include "data.hpp"
 #include "linear.hpp"
 #include "logical.hpp"
+#include "tensorbase.hpp"
 #include "types.hpp"
-
