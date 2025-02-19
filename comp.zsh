2bf4722789f856d2836d008e931f0183886650d2
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-02-19 12:39:48 +0100

    stuff

--- comp.zsh
+++ comp.zsh
@@ -1,1 +1,1 @@
-$(brew --prefix llvm)/bin/clang++ -std=c++20 test.cpp -o t -fopenmp -I$(brew --prefix libomp)/include -L$(brew --prefix libomp)/lib -v
+$(brew --prefix llvm)/bin/clang++ -std=c++20 test.cpp -o t -fopenmp -I$(brew --prefix libomp)/include -L$(brew --prefix libomp)/lib 
