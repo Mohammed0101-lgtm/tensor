5c5fef3553c60d2af287273fb46170bb0659fa77
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-02-21 11:30:29 +0100

    stuff

--- tensor_test.cpp
+++ tensor_test.cpp
@@ -3,35 +3,41 @@
 
 
 TEST(TensorTest, StorageTest) {
-  tensor<int>      t({5}, {1, 2, 3, 4, 5});
+  tensor<int> t({5}, {1, 2, 3, 4, 5});
+
   std::vector<int> expected = {1, 2, 3, 4, 5};
+
   EXPECT_EQ(t.storage(), expected);
 }
 
 TEST(TensorTest, IteratorsTest) {
   tensor<int> t({5}, {1, 2, 3, 4, 5});
+  auto        it = t.begin();
+  ++it;
+
   EXPECT_EQ(*t.begin(), 1);
   EXPECT_EQ(*(t.end() - 1), 5);
 
-  auto it = t.begin();
-  ++it;
   EXPECT_EQ(*it, 2);
 }
 
 TEST(TensorTest, ConstIteratorsTest) {
   const tensor<int> t({3}, {10, 20, 30});
+
   EXPECT_EQ(*t.begin(), 10);
   EXPECT_EQ(*(t.end() - 1), 30);
 }
 
 TEST(TensorTest, ReverseIteratorsTest) {
   tensor<int> t({5}, {1, 2, 3, 4, 5});
+
   EXPECT_EQ(*t.rbegin(), 5);
   EXPECT_EQ(*(t.rend() - 1), 1);
 }
 
 TEST(TensorTest, ConstReverseIteratorsTest) {
   const tensor<int> t({3}, {10, 20, 30});
+
   EXPECT_EQ(*t.rbegin(), 30);
   EXPECT_EQ(*(t.rend() - 1), 10);
 }
@@ -56,39 +62,158 @@ TEST(TensorTest, DataTypeConversionTest) {
 
 TEST(TensorTest, ShapeTest) {
   tensor<int> t({2, 2}, {1, 2, 3, 4});
+
   EXPECT_EQ(t.shape(), (std::vector<long long>{2, 2}));
 }
 
 TEST(TensorTest, StridesTest) {
   tensor<int> t({2, 2}, {1, 2, 3, 4});
+
   EXPECT_EQ(t.strides(), (std::vector<long long>{2, 1}));
 }
 
 TEST(TensorTest, DeviceTest) {
-    tensor<int> t({4}, {1, 2, 3, 4});
-    tensor<int> q({4}, {1, 2, 3, 4}, Device::CUDA);
-    EXPECT_EQ(t.device(), Device::CPU);
-    EXPECT_EQ(q.device(), Device::CUDA);
+  tensor<int> t({4}, {1, 2, 3, 4});
+  tensor<int> q({4}, {1, 2, 3, 4}, Device::CUDA);
+
+  EXPECT_EQ(t.device(), Device::CPU);
+  EXPECT_EQ(q.device(), Device::CUDA);
 }
 
 TEST(TensorTest, NdimsTest) {
-    tensor<int> t({4}, {1, 2, 3, 4});
-    tensor<int> q({1, 3}, {1, 2, 3});
-    int expected_1 = 1;
-    int expected_2 = 2; 
-    EXPECT_EQ(t.n_dims(), expected_1);
-    EXPECT_EQ(q.n_dims(), expected_2);
+  tensor<int> t({4}, {1, 2, 3, 4});
+  tensor<int> q({1, 3}, {1, 2, 3});
+
+  int expected_1 = 1;
+  int expected_2 = 2;
+
+  EXPECT_EQ(t.n_dims(), expected_1);
+  EXPECT_EQ(q.n_dims(), expected_2);
 }
 
 TEST(TensorTest, SizeTest) {
-    tensor<int> t({4}, {1, 2, 3, 4});
-    tensor<int> q({3, 3}, {1, 1, 8, 1, 3, 4, 5, 6, 6});
+  tensor<int> t({4}, {1, 2, 3, 4});
+  tensor<int> q({3, 3}, {1, 1, 8, 1, 3, 4, 5, 6, 6});
 
-    int expected_total1 = 4;  
-    int expected_total2 = 9;  
+  int expected_total1 = 4;
+  int expected_total2 = 9;
 
-    EXPECT_EQ(t.size(1), expected_total1);  
-    EXPECT_EQ(q.size(0), expected_total2);  
+  EXPECT_EQ(t.size(1), expected_total1);
+  EXPECT_EQ(q.size(0), expected_total2);
+}
+
+TEST(TensorTest, CapacityTest) {
+  tensor<int> a({2, 5});
+  tensor<int> b({3, 3});
+  tensor<int> c({3, 6, 4, 5});
+  tensor<int> d({9, 10});
+  tensor<int> e({3, 4, 2});
+
+  int expected1 = 10;
+  int expected2 = 9;
+  int expected3 = 360;
+  int expected4 = 90;
+  int expected5 = 24;
+
+  EXPECT_EQ(a.capacity(), expected1);
+  EXPECT_EQ(b.capacity(), expected2);
+  EXPECT_EQ(c.capacity(), expected3);
+  EXPECT_EQ(d.capacity(), expected4);
+  EXPECT_EQ(e.capacity(), expected5);
+}
+
+TEST(TensorTest, CountNonZeros) {
+  tensor<int> t({10}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+  tensor<int> q;
+  tensor<int> a = t.zeros(t.shape());
+
+  int expected  = 9;
+  int expected1 = 0;
+  int expected2 = 0;
+
+  EXPECT_EQ(t.count_nonzero(), expected);
+  EXPECT_EQ(a.count_nonzero(), expected2);
+  EXPECT_EQ(q.count_nonzero(), expected1);
+}
+
+TEST(TensorTest, ZerosTest) {
+  tensor<int> t;
+
+  t.zeros_({10});
+
+  EXPECT_EQ(t.count_nonzero(), 0);
+}
+
+TEST(TensorTest, LcmTest) {
+  tensor<int> t({2, 2}, {1, 2, 3, 4});
+
+  int expected = 12;
+
+  EXPECT_EQ(t.lcm(), expected);
+}
+
+TEST(TensorTest, AtTest) {
+  tensor<int> t({2, 3}, {1, 2, 3, 4, 5, 6});
+
+  int expected  = 2;
+  int expected1 = 6;
+
+  EXPECT_EQ(t.at({0, 1}), expected);
+  EXPECT_EQ(t.at({1, 2}), expected1);
+}
+
+TEST(TensorTest, LinearAccessOperatorTest) {
+  tensor<int> t({2, 3}, {1, 2, 3, 4, 5, 6});
+
+  int expected = 4;
+
+  EXPECT_EQ(t[3], expected);
+}
+
+TEST(TensorTest, MultiDimensionalAccessOperator) {
+  tensor<int> t({2, 3}, {1, 2, 3, 4, 5, 6});
+
+  int expected  = 6;
+  int expected1 = 2;
+
+  EXPECT_EQ(t({0, 1}), expected1);
+  EXPECT_EQ(t({1, 2}), expected);
+}
+
+TEST(TensorTest, EmptyTest) {
+  tensor<int> t;
+  tensor<int> q({1, 2}, {1, 2});
+
+  EXPECT_TRUE(t.empty());
+  EXPECT_FALSE(q.empty());
+}
+
+TEST(TensorTest, EqualOperatorTest) {
+  tensor<int> t;
+  tensor<int> q;
+
+  tensor<int> a({1, 2}, {1, 2});
+  tensor<int> b({2, 1}, {1, 2});
+  tensor<int> c({1, 2}, {1, 2});
+
+  EXPECT_TRUE(t == q);
+  EXPECT_TRUE(a == c);
+  EXPECT_FALSE(t == a);
+  EXPECT_FALSE(a == b);
+}
+
+TEST(TensorTest, NotEqualOperatorTest) {
+  tensor<int> t;
+  tensor<int> q;
+
+  tensor<int> a({1, 2}, {1, 2});
+  tensor<int> b({2, 1}, {1, 2});
+  tensor<int> c({1, 2}, {1, 2});
+
+  EXPECT_TRUE(a != b);
+  EXPECT_TRUE(t != a);
+  EXPECT_FALSE(t != q);
+  EXPECT_FALSE(a != c);
 }
 
 int main(int argc, char** argv) {
