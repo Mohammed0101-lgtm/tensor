40af7ed622c9fb4163ec97f6f8e49515fe04cfe1
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-03-13 18:01:53 +0000

    stuff

---
+++ src/arithmetic/frac.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../tensorbase.hpp"
+
+template <class _Tp>
+tensor<_Tp>& tensor<_Tp>::frac_() {
+#if defined(__ARM_NEON)
+  return this->neon_frac_();
+#endif
+
+#pragma omp parallel
+  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
+    this->__data_[__i] = static_cast<value_type>(this->__frac(this->__data_[__i]));
+
+  return *this;
+}
+
+template <class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::frac_() const {
+#if defined(__ARM_NEON)
+  return this->neon_frac_();
+#endif
+
+#pragma omp parallel
+  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
+    this->__data_[__i] = static_cast<value_type>(this->__frac(this->__data_[__i]));
+
+  return *this;
+}
+
+template <class _Tp>
+inline tensor<_Tp> tensor<_Tp>::frac() const {
+  __self __ret = this->clone();
+  __ret.frac_();
+  return __ret;
+}
