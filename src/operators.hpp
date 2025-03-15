12f453390d32187d2101e7e5ed71e8afb38bd84c
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-03-15 17:17:53 +0000

    stuff

--- src/operators.hpp
+++ src/operators.hpp
@@ -82,7 +82,7 @@ tensor<_Tp> tensor<_Tp>::operator*(const value_type __val) const {
 template <class _Tp>
 tensor<_Tp> tensor<_Tp>::operator*(const tensor& __other) const {
   static_assert(has_times_operator_v<value_type>);
-  assert(this->__shape_ == __other.shape());
+  assert(__equal_shape(this->shape(), __other.shape()));
   data_t __d(this->__data_.size());
 
 #pragma omp parallel
@@ -96,7 +96,7 @@ template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::operator+=(const tensor& __other) const {
   static_assert(has_plus_operator_v<value_type>);
 
-  assert(this->__shape_ == __other.shape());
+  assert(__equal_shape(this->shape(), __other.shape()));
 
 #pragma omp parallel
   for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] += __other[__i];
@@ -157,7 +157,7 @@ tensor<_Tp>& tensor<_Tp>::operator-=(const tensor& __other) const {
   return this->neon_operator_minus_eq(__other);
 #endif
   static_assert(has_minus_operator_v<value_type>);
-  assert(this->__shape_ == __other.shape());
+  assert(__equal_shape(this->shape(), __other.shape()));
 
 #pragma omp parallel
   for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] -= __other[__i];
@@ -171,7 +171,7 @@ tensor<_Tp>& tensor<_Tp>::operator*=(const tensor& __other) const {
   return this->neon_operator_times_eq(__other);
 #endif
   static_assert(has_times_operator_v<value_type>);
-  assert(this->__shape_ == __other.shape());
+  assert(__equal_shape(this->shape(), __other.shape()));
 
 #pragma omp parallel
   for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] *= __other[__i];
@@ -215,7 +215,7 @@ inline tensor<_Tp>& tensor<_Tp>::operator=(const tensor& __other) const {
 template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::operator/=(const tensor& __other) const {
   static_assert(has_divide_operator_v<value_type>);
-  assert(this->__shape_ == __other.shape());
+  assert(__equal_shape(this->shape(), __other.shape()));
 
 #pragma omp parallel
   for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] /= __other[__i];
@@ -240,7 +240,7 @@ tensor<_Tp> tensor<_Tp>::operator/(const tensor& __other) const {
   if (__other.count_nonzero(0) != __other.size(0))
     throw std::invalid_argument("Cannot divide by zero : undefined operation");
 
-  assert(this->__shape_ == __other.shape());
+  assert(__equal_shape(this->shape(), __other.shape()));
 
   data_t __d(this->__data_.size());
 
@@ -266,8 +266,10 @@ tensor<_Tp>& tensor<_Tp>::operator-=(const_reference __val) const {
 
 template <class _Tp>
 bool tensor<_Tp>::operator==(const tensor& __other) const {
-  if (this->__shape_ != __other.shape() || this->__strides_ != __other.strides()) return false;
-  return (this->__data_ == __other.storage());
+  if (this->__equal_shape(this->shape(), __other.shape()) && this->__strides == __other.strides() &&
+      this->__data_ == __other.storage())
+    return true;
+  return false;
 }
 
 template <class _Tp>
