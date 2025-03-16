37355cb7430ab61f4128206b0f8cbd90066b2084
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-03-16 18:14:52 +0000

    stuff

--- src/compare.hpp
+++ src/compare.hpp
@@ -4,8 +4,7 @@
 
 template <class _Tp>
 tensor<bool> tensor<_Tp>::not_equal(const tensor& __other) const {
-  if (!std::is_integral_v<value_type> && !std::is_scalar_v<value_type>)
-    throw std::runtime_error("Cannot compare non-integral or scalar value");
+  static_assert(has_not_equal_operator_v<value_type>, "Value type must have an equal operator");
 
   assert(__equal_shape(this->shape(), __other.shape()) && "not_equal : tensor shapes");
   std::vector<bool> __ret(this->__data_.size());
@@ -19,9 +18,7 @@ tensor<bool> tensor<_Tp>::not_equal(const tensor& __other) const {
 
 template <class _Tp>
 tensor<bool> tensor<_Tp>::not_equal(const value_type __val) const {
-  if (!std::is_integral_v<value_type> && !std::is_scalar_v<value_type>)
-    throw std::runtime_error("Cannot compare non-integral or scalar value");
-
+  static_assert(has_equal_operator_v<value_type>, "Value type must have an equal operator");
   std::vector<bool> __ret(this->__data_.size());
 
 #pragma omp parallel
@@ -33,9 +30,7 @@ tensor<bool> tensor<_Tp>::not_equal(const value_type __val) const {
 
 template <class _Tp>
 tensor<bool> tensor<_Tp>::less(const tensor& __other) const {
-  if (!std::is_integral_v<value_type> && !std::is_scalar_v<value_type>)
-    throw std::runtime_error("Cannot compare non-integral or scalar value");
-
+  static_assert(has_less_operator_v<value_type>, "Value type must have a less operator");
   assert(__equal_shape(this->shape(), __other.shape()));
   std::vector<bool> __ret(this->__data_.size());
 
@@ -48,9 +43,7 @@ tensor<bool> tensor<_Tp>::less(const tensor& __other) const {
 
 template <class _Tp>
 tensor<bool> tensor<_Tp>::less(const value_type __val) const {
-  if (!std::is_integral_v<value_type> && !std::is_scalar_v<value_type>)
-    throw std::runtime_error("Cannot compare non-integral or scalar value");
-
+  static_assert(has_less_operator_v<value_type>, "Value type must have a less operator");
   std::vector<bool> __ret(this->__data_.size());
 
 #pragma omp parallel
@@ -67,9 +60,7 @@ tensor<bool> tensor<_Tp>::greater(const tensor& __other) const {
 
 template <class _Tp>
 tensor<bool> tensor<_Tp>::greater(const value_type __val) const {
-  if (!std::is_arithmetic_v<value_type>)
-    throw std::logic_error("Cannot compare non arithmetic values");
-
+  static_assert(has_greater_operator_v<value_type>, "Value type must have a less operator");
   std::vector<bool> __ret(this->__data_.size());
 
 #pragma omp parallel
@@ -96,9 +87,7 @@ tensor<bool> tensor<_Tp>::less_equal(const tensor& __other) const {
 #if defined(__ARM_NEON)
   return this->neon_less_equal(__other);
 #endif
-  if (!std::is_arithmetic_v<value_type>)
-    throw std::runtime_error("Cannot compare non-numeric values");
-
+  static_assert(has_less_equal_operator_v<value_type>, "Value type must have a less equal operator");
   assert(__equal_shape(this->shape(), __other.shape()));
   std::vector<bool> __ret(this->__data_.size());
 
@@ -114,9 +103,7 @@ tensor<bool> tensor<_Tp>::less_equal(const value_type __val) const {
 #if defined(__ARM_NEON)
   return this->neon_less_equal(__val);
 #endif
-  if (!std::is_arithmetic_v<value_type>)
-    throw std::logic_error("Cannot compare non arithmetic values");
-
+  static_assert(has_less_equal_operator_v<value_type>, "Value type must have a less equal operator");
   std::vector<bool> __ret(this->__data_.size());
 
 #pragma omp parallel
