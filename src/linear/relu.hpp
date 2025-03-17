30a17c6420556cf7b1b2d7d8b01fc79621e7267f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-03-17 18:30:30 +0000

    stuff

--- src/linear/relu.hpp
+++ src/linear/relu.hpp
@@ -4,9 +4,7 @@
 
 template <class _Tp>
 tensor<_Tp> tensor<_Tp>::relu() const {
-  __self __ret = this->clone();
-  __ret.relu_();
-  return __ret;
+  return clamp_min(value_type(0));
 }
 
 template <class _Tp>
@@ -14,47 +12,7 @@ tensor<_Tp>& tensor<_Tp>::relu_() {
 #if defined(__ARM_NEON)
   return this->neon_relu_();
 #endif
-  if constexpr (std::is_unsigned_v<value_type>) return *this;
-
-  index_type __s = this->__data_.size();
-  index_type __i = 0;
-
-#pragma omp parallel
-
-#ifdef __CUDACC__
-  if (this->__is_cuda_tensor) {
-    pointer __d_data = thrust::raw_pointer_cast(this->__data_.data());
-    thrust::transform(thrust::device, __d_data, d_data + __s, __d_data,
-                      [] __device__(value_type __x) { return max(__x, value_type(0)); });
-    return;
-  }
-
-#elif defined(__SSE__)
-  if constexpr (std::is_same_v<value_type, _f32>) {
-    __m128 __zero = _mm_setzero_ps();
-
-    for (; __i + 4 <= __s; __i += 4) {
-      __m128 __x      = _mm_loadu_ps(&this->__data_[__i]);
-      __m128 __result = _mm_max_ps(__x, __zero);
-      _mm_storeu_ps(&this->__data_[__i], __result);
-    }
-  }
-
-#elif defined(__AVX__)
-  if constexpr (std::is_same_v<value_type, _f32>) {
-    __m256 __zero = _mm256_setzero_ps();
-
-    for (; __i + _AVX_REG_WIDTH <= __s; __i += _AVX_REG_WIDTH) {
-      __m256 __x      = _mm256_loadu_ps(&this->__data_[__i]);
-      __m256 __result = _mm256_max_ps(__x, __zero);
-      _mm256_storeu_ps(&this->__data_[__i], __result);
-    }
-  }
-#endif
-
-  for (__i = 0; __i < __s; ++__i) this->__data_[__i] = std::max(this->__data_[__i], value_type(0));
-
-  return *this;
+  return clamp_min_(value_type(0));
 }
 
 template <class _Tp>
@@ -62,47 +20,7 @@ inline const tensor<_Tp>& tensor<_Tp>::relu_() const {
 #if defined(__ARM_NEON)
   return this->neon_relu_();
 #endif
-  if constexpr (std::is_unsigned_v<value_type>) return *this;
-
-  index_type __s = this->__data_.size();
-  index_type __i = 0;
-
-#pragma omp parallel
-
-#ifdef __CUDACC__
-  if (this->__is_cuda_tensor) {
-    pointer __d_data = thrust::raw_pointer_cast(this->__data_.data());
-    thrust::transform(thrust::device, __d_data, d_data + __s, __d_data,
-                      [] __device__(value_type __x) { return max(__x, value_type(0)); });
-    return;
-  }
-
-#elif defined(__SSE__)
-  if constexpr (std::is_same_v<value_type, _f32>) {
-    __m128 __zero = _mm_setzero_ps();
-
-    for (; __i + 4 <= __s; __i += 4) {
-      __m128 __x      = _mm_loadu_ps(&this->__data_[__i]);
-      __m128 __result = _mm_max_ps(__x, __zero);
-      _mm_storeu_ps(&this->__data_[__i], __result);
-    }
-  }
-
-#elif defined(__AVX__)
-  if constexpr (std::is_same_v<value_type, _f32>) {
-    __m256 __zero = _mm256_setzero_ps();
-
-    for (; __i + _AVX_REG_WIDTH <= __s; __i += _AVX_REG_WIDTH) {
-      __m256 __x      = _mm256_loadu_ps(&this->__data_[__i]);
-      __m256 __result = _mm256_max_ps(__x, __zero);
-      _mm256_storeu_ps(&this->__data_[__i], __result);
-    }
-  }
-#endif
-
-  for (__i = 0; __i < __s; ++__i) this->__data_[__i] = std::max(this->__data_[__i], value_type(0));
-
-  return *this;
+  return clamp_min_(value_type(0));
 }
 
 template <class _Tp>
