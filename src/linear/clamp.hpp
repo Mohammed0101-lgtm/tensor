23bd36163451facea3f6e73eb52b637f7eccf44a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-03-19 18:10:52 +0000

    stuff

--- src/linear/clamp.hpp
+++ src/linear/clamp.hpp
@@ -4,23 +4,11 @@
 
 template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::clamp_(const_reference __min_val, const_reference __max_val) {
-  index_type __i = 0;
-
-#if defined(__AVX2__)
-  const index_type __simd_end = this->__data_.size() - (this->__data_.size() % _AVX_REG_WIDTH);
-  __m256           __min_vec  = _mm256_set1_ps(__min_val);
-  __m256           __max_vec  = _mm256_set1_ps(__max_val);
-
-  for (; __i < __simd_end; __i += _AVX_REG_WIDTH) {
-    __m256 __data_vec = _mm256_loadu_ps(&this->__data_[__i]);
-    __m256 __clamped  = _mm256_min_ps(_mm256_max_ps(data_vec, __min_vec), __max_vec);
-
-    _mm256_storeu_ps(&this->__data_[__i], __clamped);
-  }
-
-#elif defined(__ARM_NEON)
+#if defined(__ARM_NEON)
   return this->neon_clamp_(__min_val, __max_val);
 #endif
+  index_type __i = 0;
+
 #pragma omp parallel
   for (; __i < this->__data_.size(); ++__i) {
     this->__data_[__i] = std::max(__min_val, this->__data_[__i]);
@@ -33,23 +21,11 @@ tensor<_Tp>& tensor<_Tp>::clamp_(const_reference __min_val, const_reference __ma
 template <class _Tp>
 inline const tensor<_Tp>& tensor<_Tp>::clamp_(const_reference __min_val,
                                               const_reference __max_val) const {
-  index_type __i = 0;
 
-#if defined(__AVX2__)
-  const index_type __simd_end = this->__data_.size() - (this->__data_.size() % _AVX_REG_WIDTH);
-  __m256           __min_vec  = _mm256_set1_ps(__min_val);
-  __m256           __max_vec  = _mm256_set1_ps(__max_val);
-
-  for (; __i < __simd_end; __i += _AVX_REG_WIDTH) {
-    __m256 __data_vec = _mm256_loadu_ps(&this->__data_[__i]);
-    __m256 __clamped  = _mm256_min_ps(_mm256_max_ps(data_vec, __min_vec), __max_vec);
-
-    _mm256_storeu_ps(&this->__data_[__i], __clamped);
-  }
-
-#elif defined(__ARM_NEON)
+#if defined(__ARM_NEON)
   return this->neon_clamp_(__min_val, __max_val);
 #endif
+  index_type __i = 0;
 #pragma omp parallel
   for (; __i < this->__data_.size(); ++__i) {
     this->__data_[__i] = std::max(__min_val, this->__data_[__i]);
