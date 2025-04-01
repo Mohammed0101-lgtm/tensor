4c926be2ece70cfde22593719bf22e18881b0bfc
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-01 23:46:32 +0000

    stuff

--- src/simd/linear/transpose.hpp
+++ src/simd/linear/transpose.hpp
@@ -4,96 +4,96 @@
 
 template <class _Tp>
 tensor<_Tp> tensor<_Tp>::neon_transpose() const {
-  if (!__equal_shape(this->__shape_, shape_type({this->__shape_[0], this->__shape_[1]})))
-    throw __shape_error__("Matrix transposition can only be done on 2D tensors");
+  if (!equal_shape(this->shape_, shape_type({this->shape_[0], this->shape_[1]})))
+    throw shape_error("Matrix transposition can only be done on 2D tensors");
 
-  tensor           __ret({this->__shape_[1], this->__shape_[0]});
-  const index_type __rows = this->__shape_[0];
-  const index_type __cols = this->__shape_[1];
+  tensor           ret({this->shape_[1], this->shape_[0]});
+  const index_type rows = this->shape_[0];
+  const index_type cols = this->shape_[1];
 
   if constexpr (std::is_same_v<_Tp, _f32>) {
-    for (index_type __i = 0; __i < __rows; __i += _ARM64_REG_WIDTH) {
-      for (index_type __j = 0; __j < __cols; __j += _ARM64_REG_WIDTH) {
-        if (__i + _ARM64_REG_WIDTH <= __rows && __j + _ARM64_REG_WIDTH <= __cols) {
-          float32x4x4_t __input;
+    for (index_type i = 0; i < rows; i += _ARM64_REG_WIDTH) {
+      for (index_type j = 0; j < cols; j += _ARM64_REG_WIDTH) {
+        if (i + _ARM64_REG_WIDTH <= rows and j + _ARM64_REG_WIDTH <= cols) {
+          float32x4x4_t input;
 
-          for (index_type __k = 0; __k < _ARM64_REG_WIDTH; ++__k)
-            __input.val[__k] = vld1q_f32(
-                reinterpret_cast<const _f32*>(&this->__data_[(__i + __k) * __cols + __j]));
+          for (index_type k = 0; k < _ARM64_REG_WIDTH; ++k)
+            input.val[k] =
+                vld1q_f32(reinterpret_cast<const _f32*>(&this->data_[(i + k) * cols + j]));
 
-          float32x4x4_t __output = vld4q_f32(reinterpret_cast<const _f32*>(&__input));
+          float32x4x4_t output = vld4q_f32(reinterpret_cast<const _f32*>(&input));
 
-          for (index_type __k = 0; __k < _ARM64_REG_WIDTH; ++__k)
-            vst1q_f32(&__ret.__data_[(__j + __k) * __rows + __i], __output.val[__k]);
+          for (index_type k = 0; k < _ARM64_REG_WIDTH; ++k)
+            vst1q_f32(&ret.data_[(j + k) * rows + i], output.val[k]);
         } else {
-          for (index_type __ii = __i;
-               __ii < std::min(static_cast<index_type>(__i + _ARM64_REG_WIDTH), __rows); ++__ii) {
-            for (index_type __jj = __j;
-                 __jj < std::min(static_cast<index_type>(__j + _ARM64_REG_WIDTH), __cols); ++__jj) {
-              __ret.at({__jj, __ii}) = this->at({__ii, __jj});
+          for (index_type ii = i;
+               ii < std::min(static_cast<index_type>(i + _ARM64_REG_WIDTH), rows); ++ii) {
+            for (index_type jj = j;
+                 jj < std::min(static_cast<index_type>(j + _ARM64_REG_WIDTH), cols); ++jj) {
+              ret.at({jj, ii}) = this->at({ii, jj});
             }
           }
         }
       }
     }
   } else if constexpr (std::is_signed_v<value_type>) {
-    for (index_type __i = 0; __i < __rows; __i += _ARM64_REG_WIDTH) {
-      for (index_type __j = 0; __j < __cols; __j += _ARM64_REG_WIDTH) {
-        if (__i + _ARM64_REG_WIDTH <= __rows && __j + _ARM64_REG_WIDTH <= __cols) {
-          int32x4x4_t __input;
+    for (index_type i = 0; i < rows; i += _ARM64_REG_WIDTH) {
+      for (index_type j = 0; j < cols; j += _ARM64_REG_WIDTH) {
+        if (i + _ARM64_REG_WIDTH <= rows and j + _ARM64_REG_WIDTH <= cols) {
+          int32x4x4_t input;
 
-          for (index_type __k = 0; __k < _ARM64_REG_WIDTH; ++__k)
-            __input.val[__k] = vld1q_s32(
-                reinterpret_cast<const _s32*>(&this->__data_[(__i + __k) * __cols + __j]));
+          for (index_type k = 0; k < _ARM64_REG_WIDTH; ++k)
+            input.val[k] =
+                vld1q_s32(reinterpret_cast<const _s32*>(&this->data_[(i + k) * cols + j]));
 
-          int32x4x4_t __output = vld4q_s32(reinterpret_cast<const _s32*>(&__input));
+          int32x4x4_t output = vld4q_s32(reinterpret_cast<const _s32*>(&input));
 
-          for (index_type __k = 0; __k < _ARM64_REG_WIDTH; __k++)
-            vst1q_s32(&__ret.__data_[(__j + __k) * __rows + __i], __output.val[__k]);
+          for (index_type k = 0; k < _ARM64_REG_WIDTH; k++)
+            vst1q_s32(&ret.data_[(j + k) * rows + i], output.val[k]);
         } else {
-          for (index_type __ii = __i;
-               __ii < std::min(static_cast<index_type>(__i + _ARM64_REG_WIDTH), __rows); ++__ii) {
-            for (index_type __jj = __j;
-                 __jj < std::min(static_cast<index_type>(__j + _ARM64_REG_WIDTH), __cols); ++__jj) {
-              __ret.at({__jj, __ii}) = this->at({__ii, __jj});
+          for (index_type ii = i;
+               ii < std::min(static_cast<index_type>(i + _ARM64_REG_WIDTH), rows); ++ii) {
+            for (index_type jj = j;
+                 jj < std::min(static_cast<index_type>(j + _ARM64_REG_WIDTH), cols); ++jj) {
+              ret.at({jj, ii}) = this->at({ii, jj});
             }
           }
         }
       }
     }
   } else if constexpr (std::is_unsigned_v<value_type>) {
-    for (index_type __i = 0; __i < __rows; __i += _ARM64_REG_WIDTH) {
-      for (index_type __j = 0; __j < __cols; __j += _ARM64_REG_WIDTH) {
-        if (__i + _ARM64_REG_WIDTH <= __rows && __j + _ARM64_REG_WIDTH <= __cols) {
-          uint32x4x4_t __input;
+    for (index_type i = 0; i < rows; i += _ARM64_REG_WIDTH) {
+      for (index_type j = 0; j < cols; j += _ARM64_REG_WIDTH) {
+        if (i + _ARM64_REG_WIDTH <= rows and j + _ARM64_REG_WIDTH <= cols) {
+          uint32x4x4_t input;
 
-          for (index_type __k = 0; __k < _ARM64_REG_WIDTH; ++__k)
-            __input.val[__k] = vld1q_u32(
-                reinterpret_cast<const _u32*>(&this->__data_[(__i + __k) * __cols + __j]));
+          for (index_type k = 0; k < _ARM64_REG_WIDTH; ++k)
+            input.val[k] =
+                vld1q_u32(reinterpret_cast<const _u32*>(&this->data_[(i + k) * cols + j]));
 
-          uint32x4x4_t __output = vld4q_u32(reinterpret_cast<const _u32*>(&__input));
+          uint32x4x4_t output = vld4q_u32(reinterpret_cast<const _u32*>(&input));
 
-          for (index_type __k = 0; __k < _ARM64_REG_WIDTH; ++__k)
-            vst1q_u32(&__ret.__data_[(__j + __k) * __rows + __i], __output.val[__k]);
+          for (index_type k = 0; k < _ARM64_REG_WIDTH; ++k)
+            vst1q_u32(&ret.data_[(j + k) * rows + i], output.val[k]);
         } else {
-          for (index_type __ii = __i;
-               __ii < std::min(static_cast<index_type>(__i + _ARM64_REG_WIDTH), __rows); ++__ii) {
-            for (index_type __jj = __j;
-                 __jj < std::min(static_cast<index_type>(__j + _ARM64_REG_WIDTH), __cols); ++__jj) {
-              __ret.at({__jj, __ii}) = this->at({__ii, __jj});
+          for (index_type ii = i;
+               ii < std::min(static_cast<index_type>(i + _ARM64_REG_WIDTH), rows); ++ii) {
+            for (index_type jj = j;
+                 jj < std::min(static_cast<index_type>(j + _ARM64_REG_WIDTH), cols); ++jj) {
+              ret.at({jj, ii}) = this->at({ii, jj});
             }
           }
         }
       }
     }
   } else {
-    index_type __i = 0;
+    index_type i = 0;
 #pragma omp parallel
-    for (; __i < __rows; ++__i) {
-      index_type __j = 0;
-      for (; __j < __cols; ++__j) __ret.at({__j, __i}) = this->at({__i, __j});
+    for (; i < rows; ++i) {
+      index_type j = 0;
+      for (; j < cols; ++j) ret.at({j, i}) = this->at({i, j});
     }
   }
 
-  return __ret;
+  return ret;
 }
