9d9ca5ab3386b6e33862eb4d6785f01f467d01b3
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-02 15:07:34 +0000

    stuff

--- src/simd/linear/relu.hpp
+++ src/simd/linear/relu.hpp
@@ -8,46 +8,46 @@ tensor<_Tp>& tensor<_Tp>::neon_relu_() {
 }
 /*
 template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_clipped_relu_(const value_type __clip_limit) {
+tensor<_Tp>& tensor<_Tp>::neon_clipped_relu_(const value_type clip_limit) {
   if constexpr (std::is_unsigned_v<value_type>) return *this;
 
-  index_type __s = this->__data_.size();
-  index_type __i = 0;
+  index_type s = this->data_.size();
+  index_type i = 0;
 
   if constexpr (std::is_same_v<value_type, _f32>) {
-    const neon_f32 __vZero = vdupq_n_f32(0.0f);
-    const neon_f32 __vClip = vdupq_n_f32(__clip_limit);
+    const neon_f32 vZero = vdupq_n_f32(0.0f);
+    const neon_f32 vClip = vdupq_n_f32(clip_limit);
 
-    for (; __i + _ARM64_REG_WIDTH <= __s; __i += _ARM64_REG_WIDTH) {
-      neon_f32 __v = vld1q_f32(reinterpret_cast<const _f32*>(&this->__data_[__i]));
-      __v          = vminq_f32(vmaxq_f32(__v, __vZero), __vClip);
+    for (; i + _ARM64_REG_WIDTH <= s; i += _ARM64_REG_WIDTH) {
+      neon_f32 v = vld1q_f32(reinterpret_cast<const _f32*>(&this->data_[i]));
+      v          = vminq_f32(vmaxq_f32(v, vZero), vClip);
 
-      vst1q_f32(&this->__data_[__i], __v);
+      vst1q_f32(&this->data_[i], v);
     }
   } else if constexpr (std::is_same_v<value_type, _s32>) {
-    const neon_s32 __vZero = vdupq_n_s32(0);
-    const neon_s32 __vClip = vdupq_n_s32(__clip_limit);
+    const neon_s32 vZero = vdupq_n_s32(0);
+    const neon_s32 vClip = vdupq_n_s32(clip_limit);
 
-    for (; __i + _ARM64_REG_WIDTH <= __s; __i += _ARM64_REG_WIDTH) {
-      neon_s32 __v = vld1q_s32(reinterpret_cast<const _s32*>(&this->__data_[__i]));
-      __v          = vminq_s32(vmaxq_s32(__v, __vZero), __vClip);
+    for (; i + _ARM64_REG_WIDTH <= s; i += _ARM64_REG_WIDTH) {
+      neon_s32 v = vld1q_s32(reinterpret_cast<const _s32*>(&this->data_[i]));
+      v          = vminq_s32(vmaxq_s32(v, vZero), vClip);
 
-      vst1q_s32(&this->__data_[__i], __v);
+      vst1q_s32(&this->data_[i], v);
     }
   }
 #pragma omp parallel
-  for (; __i < __s; ++__i)
-    this->__data_[__i] = std::min(std::max(this->__data_[__i], value_type(0)), __clip_limit);
+  for (; i < s; ++i)
+    this->data_[i] = std::min(std::max(this->data_[i], value_type(0)), clip_limit);
 
   return *this;
 }
 */
 template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_clipped_relu_(const value_type __clip_limit) {
+tensor<_Tp>& tensor<_Tp>::neon_clipped_relu_(const value_type clip_limit) {
   if constexpr (std::is_unsigned_v<value_type>) return *this;
 
   this->neon_clamp_(value_type(0), std::numeric_limits<value_type>::max());
-  this->neon_clamp_(std::numeric_limits<value_type>::lowest(), __clip_limit);
+  this->neon_clamp_(std::numeric_limits<value_type>::lowest(), clip_limit);
 
   return *this;
 }
