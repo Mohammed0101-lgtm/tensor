e71928dff18fb3745f2528c003f1b43de31e7a3c
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-04 10:52:25 +0000

    stuff

--- src/arithmetic/sin.hpp
+++ src/arithmetic/sin.hpp
@@ -7,11 +7,11 @@ tensor<_Tp>& tensor<_Tp>::sin_() {
 #if defined(__ARM_NEON)
   return this->neon_sin_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::sin(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::sin(this->data_[i]));
 
   return *this;
 }
@@ -21,34 +21,34 @@ inline const tensor<_Tp>& tensor<_Tp>::sin_() const {
 #if defined(__ARM_NEON)
   return this->neon_sin_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::sin(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::sin(this->data_[i]));
 
   return *this;
 }
 
 template <class _Tp>
 inline tensor<_Tp> tensor<_Tp>::sin() const {
-  __self __ret = this->clone();
-  __ret.sin_();
-  return __ret;
+  self ret = this->clone();
+  ret.sin_();
+  return ret;
 }
 
 template <class _Tp>
 inline tensor<_Tp> tensor<_Tp>::asin() const {
-  __self __ret = this->clone();
-  __ret.asin_();
-  return __ret;
+  self ret = this->clone();
+  ret.asin_();
+  return ret;
 }
 
 template <class _Tp>
 inline tensor<_Tp> tensor<_Tp>::atan() const {
-  __self __ret = this->clone();
-  __ret.atan_();
-  return __ret;
+  self ret = this->clone();
+  ret.atan_();
+  return ret;
 }
 
 template <class _Tp>
@@ -56,14 +56,14 @@ tensor<_Tp>& tensor<_Tp>::sinc_() {
 #if defined(__ARM_NEON)
   return this->neon_sinc_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = (std::abs(this->__data_[__i]) < 1e-6)
-                             ? static_cast<value_type>(1.0)
-                             : static_cast<value_type>(std::sin(M_PI * this->__data_[__i]) /
-                                                       (M_PI * this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] =
+        (std::abs(this->data_[i]) < 1e-6)
+            ? static_cast<value_type>(1.0)
+            : static_cast<value_type>(std::sin(M_PI * this->data_[i]) / (M_PI * this->data_[i]));
 
   return *this;
 }
@@ -73,22 +73,22 @@ inline const tensor<_Tp>& tensor<_Tp>::sinc_() const {
 #if defined(__ARM_NEON)
   return this->neon_sinc_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = (std::abs(this->__data_[__i]) < 1e-6)
-                             ? static_cast<value_type>(1.0)
-                             : static_cast<value_type>(std::sin(M_PI * this->__data_[__i]) /
-                                                       (M_PI * this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] =
+        (std::abs(this->data_[i]) < 1e-6)
+            ? static_cast<value_type>(1.0)
+            : static_cast<value_type>(std::sin(M_PI * this->data_[i]) / (M_PI * this->data_[i]));
   return *this;
 }
 
 template <class _Tp>
 inline tensor<_Tp> tensor<_Tp>::sinc() const {
-  __self __ret = this->clone();
-  __ret.sinc_();
-  return __ret;
+  self ret = this->clone();
+  ret.sinc_();
+  return ret;
 }
 
 template <class _Tp>
@@ -96,11 +96,11 @@ tensor<_Tp>& tensor<_Tp>::sinh_() {
 #if defined(__ARM_NEON)
   return this->neon_sinh_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::sinh(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::sinh(this->data_[i]));
 
   return *this;
 }
@@ -110,20 +110,20 @@ inline const tensor<_Tp>& tensor<_Tp>::sinh_() const {
 #if defined(__ARM_NEON)
   return this->neon_sinh_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::sinh(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::sinh(this->data_[i]));
 
   return *this;
 }
 
 template <class _Tp>
 inline tensor<_Tp> tensor<_Tp>::sinh() const {
-  __self __ret = this->clone();
-  __ret.sinh_();
-  return __ret;
+  self ret = this->clone();
+  ret.sinh_();
+  return ret;
 }
 
 template <class _Tp>
@@ -131,11 +131,11 @@ tensor<_Tp>& tensor<_Tp>::asinh_() {
 #if defined(__ARM_NEON)
   return this->neon_sinh_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::asinh(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::asinh(this->data_[i]));
 
   return *this;
 }
@@ -145,20 +145,20 @@ inline const tensor<_Tp>& tensor<_Tp>::asinh_() const {
 #if defined(__ARM_NEON)
   return this->neon_sinh_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::asinh(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::asinh(this->data_[i]));
 
   return *this;
 }
 
 template <class _Tp>
 inline tensor<_Tp> tensor<_Tp>::asinh() const {
-  __self __ret = this->clone();
-  __ret.asinh_();
-  return __ret;
+  self ret = this->clone();
+  ret.asinh_();
+  return ret;
 }
 
 template <class _Tp>
@@ -166,11 +166,11 @@ tensor<_Tp>& tensor<_Tp>::asin_() {
 #if defined(__ARM_NEON)
   return this->neon_asin_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::asin(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::asin(this->data_[i]));
 
   return *this;
 }
@@ -180,11 +180,11 @@ inline const tensor<_Tp>& tensor<_Tp>::asin_() const {
 #if defined(__ARM_NEON)
   return this->neon_asin_();
 #endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
 
 #pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::asin(this->__data_[__i]));
+  for (index_type i = 0; i < this->data_.size(); ++i)
+    this->data_[i] = static_cast<value_type>(std::asin(this->data_[i]));
 
   return *this;
 }
