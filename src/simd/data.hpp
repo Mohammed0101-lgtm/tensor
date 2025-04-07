eda7b02769e71802482ab10bef521775f63000b7
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-07 16:39:14 +0100

    stuff

--- src/simd/data.hpp
+++ src/simd/data.hpp
@@ -4,7 +4,9 @@
 
 template <class _Tp>
 typename tensor<_Tp>::index_type tensor<_Tp>::neon_count_nonzero(index_type dim) const {
-  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) {
+    throw type_error("Type must be arithmetic");
+  }
 
   index_type c           = 0;
   index_type local_count = 0;
@@ -31,12 +33,15 @@ typename tensor<_Tp>::index_type tensor<_Tp>::neon_count_nonzero(index_type dim)
     }
 #pragma omp parallel for reduction(+ : local_count)
     for (index_type j = i; j < this->data_.size(); ++j)
-      if (this->data_[j] != 0) ++local_count;
+      if (this->data_[j] != 0) {
+        ++local_count;
+      }
 
     c += local_count;
   } else {
-    if (dim < 0 or dim >= static_cast<index_type>(shape_.size()))
+    if (dim < 0 or dim >= static_cast<index_type>(shape_.size())) {
       throw index_error("Invalid dimension provided.");
+    }
 
     throw std::runtime_error("Dimension-specific non-zero counting is not implemented yet.");
   }
@@ -46,12 +51,15 @@ typename tensor<_Tp>::index_type tensor<_Tp>::neon_count_nonzero(index_type dim)
 
 template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::neon_zeros_(shape_type sh) {
-  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) {
+    throw type_error("Type must be arithmetic");
+  }
 
-  if (sh.empty())
+  if (sh.empty()) {
     sh = this->shape_;
-  else
+  } else {
     this->shape_ = sh;
+  }
 
   size_t s = this->computeSize(this->shape_);
   this->data_.resize(s);
@@ -61,28 +69,39 @@ tensor<_Tp>& tensor<_Tp>::neon_zeros_(shape_type sh) {
 
   if constexpr (std::is_floating_point_v<value_type>) {
     neon_f32 zero_vec = vdupq_n_f32(0.0f);
-    for (; i < simd_end; i += _ARM64_REG_WIDTH) vst1q_f32(&this->data_[i], zero_vec);
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
+      vst1q_f32(&this->data_[i], zero_vec);
+    }
   } else if constexpr (std::is_signed_v<value_type>) {
     neon_s32 zero_vec = vdupq_n_s32(0);
-    for (; i < simd_end; i += _ARM64_REG_WIDTH) vst1q_s32(&this->data_[i], zero_vec);
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
+      vst1q_s32(&this->data_[i], zero_vec);
+    }
   } else if constexpr (std::is_unsigned_v<value_type>) {
     neon_u32 zero_vec = vdupq_n_u32(0);
-    for (; i < simd_end; i += _ARM64_REG_WIDTH) vst1q_u32(&this->data_[i], zero_vec);
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
+      vst1q_u32(&this->data_[i], zero_vec);
+    }
   }
 #pragma omp parallel
-  for (; i < s; ++i) this->data_[i] = value_type(0.0);
+  for (; i < s; ++i) {
+    this->data_[i] = value_type(0.0);
+  }
 
   return *this;
 }
 
 template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::neon_ones_(shape_type sh) {
-  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) {
+    throw type_error("Type must be arithmetic");
+  }
 
-  if (sh.empty())
+  if (sh.empty()) {
     sh = this->shape_;
-  else
+  } else {
     this->shape_ = sh;
+  }
 
   size_t s = this->computeSize(this->shape_);
   this->data_.resize(s);
@@ -93,36 +112,47 @@ tensor<_Tp>& tensor<_Tp>::neon_ones_(shape_type sh) {
   if constexpr (std::is_floating_point_v<value_type>) {
     neon_f32 one_vec = vdupq_n_f32(1.0f);
 
-    for (; i < simd_end; i += _ARM64_REG_WIDTH)
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
       vst1q_f32(reinterpret_cast<_f32*>(&this->data_[i]), one_vec);
+    }
   } else if constexpr (std::is_signed_v<value_type>) {
     neon_s32 one_vec = vdupq_n_s32(1);
 
-    for (; i < simd_end; i += _ARM64_REG_WIDTH)
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
       vst1q_s32(reinterpret_cast<_s32*>(&this->data_[i]), one_vec);
+    }
   } else if constexpr (std::is_unsigned_v<value_type>) {
     neon_u32 one_vec = vdupq_n_u32(1);
 
-    for (; i < simd_end; i += _ARM64_REG_WIDTH)
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
       vst1q_u32(reinterpret_cast<_u32*>(&this->data_[i]), one_vec);
+    }
   }
 #pragma omp parallel
-  for (; i < s; ++i) this->data_[i] = value_type(1.0);
+  for (; i < s; ++i) {
+    this->data_[i] = value_type(1.0);
+  }
 
   return *this;
 }
 
 template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::neon_randomize_(const shape_type& sh, bool bounded) {
-  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) {
+    throw type_error("Type must be arithmetic");
+  }
 
-  if (bounded)
+  if (bounded) {
     assert(std::is_floating_point_v<value_type> and "Cannot bound non floating point data type");
+  }
 
-  if (sh.empty() and this->shape_.empty())
+  if (sh.empty() and this->shape_.empty()) {
     throw shape_error("randomize_ : Shape must be initialized");
+  }
 
-  if (this->shape_.empty() or this->shape_ != sh) this->shape_ = sh;
+  if (this->shape_.empty() or this->shape_ != sh) {
+    this->shape_ = sh;
+  }
 
   index_type s = this->computeSize(this->shape_);
   this->data_.resize(s);
@@ -139,14 +169,17 @@ tensor<_Tp>& tensor<_Tp>::neon_randomize_(const shape_type& sh, bool bounded) {
     for (; i + _ARM64_REG_WIDTH <= static_cast<index_type>(s); i += _ARM64_REG_WIDTH) {
       neon_f32 random_values;
 
-      if (bounded)
+      if (bounded) {
         random_values = {bounded_dist(gen), bounded_dist(gen), bounded_dist(gen),
                          bounded_dist(gen)};
-      else
+      } else {
         random_values = {unbounded_dist(gen), unbounded_dist(gen), unbounded_dist(gen),
                          unbounded_dist(gen)};
+      }
 
-      if (!bounded) random_values = vmulq_f32(random_values, vrecpeq_f32(scale));
+      if (!bounded) {
+        random_values = vmulq_f32(random_values, vrecpeq_f32(scale));
+      }
 
       vst1q_f32(&this->data_[i], random_values);
     }
@@ -174,15 +207,18 @@ tensor<_Tp>& tensor<_Tp>::neon_randomize_(const shape_type& sh, bool bounded) {
     }
   }
 #pragma omp parallel
-  for (; i < static_cast<index_type>(s); ++i)
+  for (; i < static_cast<index_type>(s); ++i) {
     this->data_[i] = value_type(bounded ? bounded_dist(gen) : unbounded_dist(gen));
+  }
 
   return *this;
 }
 
 template <class _Tp>
 tensor<_Tp>& tensor<_Tp>::neon_negative_() {
-  if (!std::is_arithmetic_v<value_type>) throw type_error("Type must be arithmetic");
+  if (!std::is_arithmetic_v<value_type>) {
+    throw type_error("Type must be arithmetic");
+  }
 
   const index_type simd_end = this->data_.size() - (this->data_.size() % _ARM64_REG_WIDTH);
   index_type       i        = 0;
@@ -210,7 +246,9 @@ tensor<_Tp>& tensor<_Tp>::neon_negative_() {
     }
   }
 #pragma omp parallel
-  for (; i < this->data_.size(); ++i) this->data_[i] = -this->data_[i];
+  for (; i < this->data_.size(); ++i) {
+    this->data_[i] = -this->data_[i];
+  }
 
   return *this;
 }
