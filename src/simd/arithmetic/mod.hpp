28e626f3f62612755609ce653e7b7f03d11d4390
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-08 12:03:38 +0100

    stuff

--- src/simd/arithmetic/mod.hpp
+++ src/simd/arithmetic/mod.hpp
@@ -3,56 +3,59 @@
 #include "tensorbase.hpp"
 
 template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_fmod_(const value_type __val) {
-  assert(std::is_floating_point_v<value_type> &&
-         "fmod : template class must be a floating point type");
-  index_type __i = 0;
+tensor<_Tp>& tensor<_Tp>::neon_fmod_(const value_type val) {
+  static_assert(std::is_floating_point_v<value_type>,
+                "fmod : template class must be a floating point type");
+  index_type i = 0;
 
   if constexpr (std::is_floating_point_v<value_type>) {
-    const index_type __simd_end = this->__data_.size() - (this->__data_.size() - _ARM64_REG_WIDTH);
-    neon_f32         __b        = vdupq_n_f32(reinterpret_cast<_f32>(__val));
-    for (; __i < __simd_end; __i += _ARM64_REG_WIDTH) {
-      neon_f32 __a         = vld1q_f32(reinterpret_cast<const _f32*>(&this->__data_[__i]));
-      neon_f32 __div       = vdivq_f32(__a, __b);
-      neon_f32 __floor_div = vrndq_f32(__div);
-      neon_f32 __mult      = vmulq_f32(__floor_div, __b);
-      neon_f32 __mod       = vsubq_f32(__a, __mult);
+    const index_type simd_end = this->data_.size() - (this->data_.size() - _ARM64_REG_WIDTH);
+    neon_f32         b        = vdupq_n_f32(reinterpret_cast<_f32>(val));
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
+      neon_f32 a         = vld1q_f32(reinterpret_cast<const _f32*>(&this->data_[i]));
+      neon_f32 div       = vdivq_f32(a, b);
+      neon_f32 floor_div = vrndq_f32(div);
+      neon_f32 mult      = vmulq_f32(floor_div, b);
+      neon_f32 mod       = vsubq_f32(a, mult);
 
-      vst1q_f32(&this->__data_[__i], __mod);
+      vst1q_f32(&this->data_[i], mod);
     }
   }
 #pragma omp parallel
-  for (; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(
-        std::fmod(static_cast<_f32>(this->__data_[__i]), static_cast<_f32>(__val)));
+  for (; i < this->data_.size(); ++i) {
+    this->data_[i] = static_cast<value_type>(
+        std::fmod(static_cast<_f32>(this->data_[i]), static_cast<_f32>(val)));
+  }
 
   return *this;
 }
 
 template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_fmod_(const tensor& __other) {
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Cannot divide two tensors of different shapes : fmax");
+tensor<_Tp>& tensor<_Tp>::neon_fmod_(const tensor& other) {
+  if (!equal_shape(this->shape(), other.shape())) {
+    throw shape_error("Cannot divide two tensors of different shapes : fmax");
+  }
 
-  index_type __i = 0;
+  index_type i = 0;
 
   if constexpr (std::is_floating_point_v<value_type>) {
-    const index_type __simd_end = this->__data_.size() - (this->__data_.size() % _ARM64_REG_WIDTH);
-    for (; __i < __simd_end; __i += _ARM64_REG_WIDTH) {
-      neon_f32 __a         = vld1q_f32(reinterpret_cast<const _f32*>(&this->__data_[__i]));
-      neon_f32 __b         = vld1q_f32(reinterpret_cast<const _f32*>(&__other[__i]));
-      neon_f32 __div       = vdivq_f32(__a, __b);
-      neon_f32 __floor_div = vrndq_f32(__div);
-      neon_f32 __mult      = vmulq_f32(__floor_div, __b);
-      neon_f32 __mod       = vsubq_f32(__a, __mult);
+    const index_type simd_end = this->data_.size() - (this->data_.size() % _ARM64_REG_WIDTH);
+    for (; i < simd_end; i += _ARM64_REG_WIDTH) {
+      neon_f32 a         = vld1q_f32(reinterpret_cast<const _f32*>(&this->data_[i]));
+      neon_f32 b         = vld1q_f32(reinterpret_cast<const _f32*>(&other[i]));
+      neon_f32 div       = vdivq_f32(a, b);
+      neon_f32 floor_div = vrndq_f32(div);
+      neon_f32 mult      = vmulq_f32(floor_div, b);
+      neon_f32 mod       = vsubq_f32(a, mult);
 
-      vst1q_f32(&this->__data_[__i], __mod);
+      vst1q_f32(&this->data_[i], mod);
     }
   }
 #pragma omp parallel
-  for (; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(
-        std::fmod(static_cast<_f32>(this->__data_[__i]), static_cast<_f32>(__other[__i])));
+  for (; i < this->data_.size(); ++i) {
+    this->data_[i] = static_cast<value_type>(
+        std::fmod(static_cast<_f32>(this->data_[i]), static_cast<_f32>(other[i])));
+  }
 
   return *this;
 }
