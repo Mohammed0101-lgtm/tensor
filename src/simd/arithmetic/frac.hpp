586a34fddfc7173df47f73dce5ff5938b8929bbb
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-16 16:38:55 +0100

    stuff

--- src/simd/arithmetic/frac.hpp
+++ src/simd/arithmetic/frac.hpp
@@ -2,48 +2,38 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::neon_frac_() {
-  if (!std::is_floating_point_v<value_type>) throw __type_error__("Type must be floating point");
-
-  index_type __i = 0;
-
-  if constexpr (std::is_same_v<value_type, _f32>) {
-    index_type __simd_end = this->__data_.size() - (this->__data_.size() % _ARM64_REG_WIDTH);
-    for (; __i < __simd_end; __i += _ARM64_REG_WIDTH) {
-      neon_f32 __data_vec = vld1q_f32(reinterpret_cast<const _f32*>(&this->__data_[__i]));
-      _f32     __vals[_ARM64_REG_WIDTH];
-      vst1q_f32(__vals, __data_vec);
-
-      __vals[0] = this->__frac(__vals[0]);
-      __vals[1] = this->__frac(__vals[1]);
-      __vals[2] = this->__frac(__vals[2]);
-      __vals[3] = this->__frac(__vals[3]);
-
-      neon_f32 __atan_vec = vld1q_f32(__vals);
-      vst1q_f32(&this->__data_[__i], __atan_vec);
+    if (!std::is_floating_point_v<value_type>)
+    {
+        throw type_error("Type must be floating point");
     }
-  }
-  if constexpr (std::is_same_v<value_type, _f64>) {
-    index_type __simd_end = this->__data_.size() - (this->__data_.size() % (_ARM64_REG_WIDTH / 2));
 
-    for (; __i < __simd_end; __i += (_ARM64_REG_WIDTH / 2)) {
-      neon_f64 __data_vec = vld1q_f64(reinterpret_cast<const _f64*>(&this->__data_[__i]));
-      _f64     __vals[_ARM64_REG_WIDTH];
-      vst1q_f64(__vals, __data_vec);
+    constexpr std::size_t simd_width = _ARM64_REG_WIDTH / sizeof(value_type);
+    static_assert(simd_width % 2 == 0, "register width must divide the size of the data type evenly");
 
-      __vals[0] = static_cast<_f64>(this->__frac(__vals[0]));
-      __vals[1] = static_cast<_f64>(this->__frac(__vals[1]));
-      __vals[2] = static_cast<_f64>(this->__frac(__vals[2]));
-      __vals[3] = static_cast<_f64>(this->__frac(__vals[3]));
+    index_type simd_end = data_.size() - (data_.size() % simd_width);
+    index_type i        = 0;
 
-      neon_f64 __atan_vec = vld1q_f64(__vals);
-      vst1q_f64(&this->__data_[__i], __atan_vec);
+    for (; i < simd_end; i += simd_width)
+    {
+        neon_type<value_type> vec = neon_load<value_type>(&data_[i]);
+        value_type            vals[simd_width];
+        neon_store<value_type>(vals, vec);
+
+        for (int j = 0; j < simd_width; j++)
+        {
+            vals[j] = frac(vals[j]);
+        }
+
+        neon_type<value_type> atan_vec = neon_load<value_type>(vals);
+        neon_store<value_type>(&data_[i], atan_vec);
     }
-  }
-#pragma omp parallel
-  for (; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(this->__frac(this->__data_[__i]));
 
-  return *this;
+    for (; i < data_.size(); ++i)
+    {
+        data_[i] = static_cast<value_type>(frac(data_[i]));
+    }
+
+    return *this;
 }
