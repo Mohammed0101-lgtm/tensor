0488eb7464ba6e8798ec54dfaf7b0a8fdbbb2175
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-18 17:21:28 +0100

    stuff

--- src/simd/linear/sigmoid.hpp
+++ src/simd/linear/sigmoid.hpp
@@ -13,11 +13,13 @@ tensor<_Tp>& tensor<_Tp>::neon_sigmoid_() {
 
     using neon_type = typename std::conditional<std::is_same_v<value_type, _f32>, neon_f32, void>::type;
 
+    constexpr std::size_t simd_width = _ARM64_REG_WIDTH / sizeof(value_type);
+    static_assert(simd_width % 2 == 0, "register width must divide the size of the data type evenly");
+    const index_type simd_end = data_.size() - (data_.size() % simd_width);
+
     if constexpr (std::is_same_v<value_type, _f32>)
     {
-        const index_type simd_end = data_.size() - (data_.size() % _ARM64_REG_WIDTH);
-
-        for (; i < simd_end; i += _ARM64_REG_WIDTH)
+        for (; i < simd_end; i += simd_width)
         {
             neon_type v         = vld1q_f32(reinterpret_cast<const _f32*>(&data_[i]));
             neon_type exp_neg_v = vexpq_f32(vnegq_f32(v));                               // e^(-x)
