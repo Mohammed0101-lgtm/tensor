9d315e6af28223c481026d10cfc5de890f1233d3
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-25 17:28:12 +0100

    stuff

--- src/simd/linear/sum.hpp
+++ src/simd/linear/sum.hpp
@@ -19,119 +19,32 @@ tensor<_Tp> tensor<_Tp>::neon_sum(const index_type axis) const {
     const index_type inner_size = size(0) / (outer_size * axis_size);
 
     const index_type simd_end = data_.size() - (data_.size() % simd_width);
+    value_type       zero(0.0f);
 
-    if constexpr (std::is_floating_point_v<value_type>)
+    for (index_type outer = 0; outer < outer_size; ++outer)
     {
-        for (index_type outer = 0; outer < outer_size; ++outer)
+        for (index_type inner = 0; inner < inner_size; ++inner)
         {
-            for (index_type inner = 0; inner < inner_size; ++inner)
+            neon_type<value_type> sum_vec = neon_dup<value_type>(&zero);
+            index_type            i       = outer * axis_size * inner_size + inner;
+            index_type            j       = 0;
+
+            for (; j < axis_size; j += simd_width)
             {
-                neon_f32   sum_vec = vdupq_n_f32(0.0f);
-                index_type i       = outer * axis_size * inner_size + inner;
-                index_type j       = 0;
-
-                for (; j + simd_width <= axis_size; j += simd_width)
-                {
-                    neon_f32 data_vec = vld1q_f32(reinterpret_cast<const _f32*>(&data_[i]));
-                    sum_vec           = vaddq_f32(sum_vec, data_vec);
-                    i += inner_size * simd_width;
-                }
-
-                _f32 sum = vaddvq_f32(sum_vec);
-
-                for (; j < axis_size; ++j)
-                {
-                    sum += data_[i];
-                    i += inner_size;
-                }
-
-                ret_data[outer * inner_size + inner] = sum;
-            }
-        }
-    }
-    else if constexpr (std::is_signed_v<value_type>)
-    {
-        for (index_type outer = 0; outer < outer_size; ++outer)
-        {
-            for (index_type inner = 0; inner < inner_size; ++inner)
-            {
-                neon_s32   sum_vec = vdupq_n_s32(0);
-                index_type i       = outer * axis_size * inner_size + inner;
-                index_type j       = 0;
-
-                for (; j + simd_width <= axis_size; j += simd_width)
-                {
-                    neon_s32 data_vec = vld1q_s32(reinterpret_cast<const _s32*>(&data_[i]));
-                    sum_vec           = vaddq_s32(sum_vec, data_vec);
-                    i += inner_size * simd_width;
-                }
-
-                _s32 sum = vaddvq_s32(sum_vec);
-
-                for (; j < axis_size; ++j)
-                {
-                    sum += data_[i];
-                    i += inner_size;
-                }
-
-                ret_data[outer * inner_size + inner] = sum;
-            }
-        }
-    }
-    else if constexpr (std::is_unsigned_v<value_type>)
-    {
-        for (index_type outer = 0; outer < outer_size; ++outer)
-        {
-            for (index_type inner = 0; inner < inner_size; ++inner)
-            {
-                neon_u32   sum_vec = vdupq_n_u32(0);
-                index_type i       = outer * axis_size * inner_size + inner;
-                index_type j       = 0;
-
-                for (; j + simd_width <= axis_size; j += simd_width)
-                {
-                    neon_u32 data_vec = vld1q_u32(reinterpret_cast<const _u32*>(&data_[i]));
-                    sum_vec           = vaddq_u32(sum_vec, data_vec);
-                    i += inner_size * simd_width;
-                }
-
-                _u32 sum = vaddvq_u32(sum_vec);
-
-                for (; j < axis_size; ++j)
-                {
-                    sum += data_[i];
-                    i += inner_size;
-                }
-
-                ret_data[outer * inner_size + inner] = sum;
-            }
-        }
-    }
-    else
-    {
-        index_type i = 0;
-        for (; i < static_cast<index_type>(data_.size()); ++i)
-        {
-            std::vector<index_type> orig(shape_.size());
-            index_type              index = i;
-            index_type              j     = static_cast<index_type>(shape_.size()) - 1;
-
-            for (; j >= 0; j--)
-            {
-                orig[j] = index % shape_[j];
-                index /= shape_[j];
+                neon_type<value_type> data_vec = neon_load<value_type>(&data_[i]);
+                sum_vec                        = neon_add<value_type>(sum_vec, data_vec);
+                i += inner_size * simd_width;
             }
 
-            orig[axis]           = 0;
-            index_type ret_index = 0;
-            index_type st        = 1;
+            value_type sum = neon_addv<value_type>(sum_vec);
 
-            for (j = static_cast<index_type>(shape_.size()) - 1; j >= 0; j--)
+            for (; j < axis_size; ++j)
             {
-                ret_index += orig[j] * st;
-                st *= ret_sh[j];
+                sum = sum + data_[i];
+                i += inner_size;
             }
-            ret_data[ret_index] += data_[i];
+
+            ret_data[outer * inner_size + inner] = sum;
         }
     }
 
