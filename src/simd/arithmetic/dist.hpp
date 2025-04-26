e88e025c12a848049550220c85d74b5c9399efa5
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-04-26 16:04:08 +0100

    stuff

--- src/simd/arithmetic/dist.hpp
+++ src/simd/arithmetic/dist.hpp
@@ -15,11 +15,9 @@ tensor<_Tp>& tensor<_Tp>::neon_dist_(const tensor& other) {
         throw shape_error("Tensors shapes must be equal");
     }
 
-    constexpr std::size_t simd_width = _ARM64_REG_WIDTH / sizeof(value_type);
-    static_assert(simd_width % 2 == 0, "register width must divide the size of the data type evenly");
     const index_type simd_end = data_.size() - (data_.size() % simd_width);
 
-    index_type i        = 0;
+    index_type i = 0;
     for (; i < simd_end; i += simd_width)
     {
         value_type dummy;
@@ -49,11 +47,9 @@ tensor<_Tp>& tensor<_Tp>::neon_dist_(const value_type val) {
         throw type_error("Type must be arithmetic");
     }
 
-    constexpr std::size_t simd_width = _ARM64_REG_WIDTH / sizeof(value_type);
-    static_assert(simd_width % 2 == 0, "register width must divide the size of the data type evenly");
     const index_type simd_end = data_.size() - (data_.size() % simd_width);
 
-    index_type i        = 0;
+    index_type i = 0;
     for (; i < simd_end; i += simd_width)
     {
         neon_type<value_type> a    = neon_load<value_type>(&data_[i]);
