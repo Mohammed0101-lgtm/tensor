d1b62a3784707521fb629262e20b259166df667f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-03 17:56:08 +0100

    stuff

--- src/linear/squeeze.hpp
+++ src/linear/squeeze.hpp
@@ -2,54 +2,60 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
-tensor<_Tp> tensor<_Tp>::unsqueeze(index_type __dim) const {
-  if (__dim < 0 || __dim > static_cast<index_type>(this->__shape_.size()))
-    throw __index_error__("Dimension out of range in unsqueeze");
+template<class _Tp>
+tensor<_Tp> tensor<_Tp>::unsqueeze(index_type dimension) const {
+    if (dimension < 0 || dimension > static_cast<index_type>(shape_.size()))
+    {
+        throw index_error("Dimension out of range in unsqueeze");
+    }
 
-  shape_type __s = this->__shape_;
-  __s.insert(__s.begin() + __dim, 1);
+    shape_type s = shape_;
+    s.insert(s.begin() + dimension, 1);
 
-  tensor __ret;
-  __ret.__shape_ = __s;
-  __ret.__data_  = this->__data_;
+    tensor ret;
+    ret.shape_ = s;
+    ret.data_  = data_;
 
-  return __ret;
+    return ret;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::unsqueeze_(index_type __dim) {
-  if (__dim < 0 || __dim > static_cast<index_type>(this->__shape_.size()))
-    throw __index_error__("Dimension out of range in unsqueeze");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::unsqueeze_(index_type dimension) {
+    if (dimension < 0 || dimension > static_cast<index_type>(shape_.size()))
+    {
+        throw index_error("Dimension out of range in unsqueeze");
+    }
 
-  this->__shape_.insert(this->__shape_.begin() + __dim, 1);
+    shape_.insert(shape_.begin() + dimension, 1);
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::unsqueeze_(index_type __dim) const {
-  if (__dim < 0 || __dim > static_cast<index_type>(this->__shape_.size()))
-    throw __index_error__("Dimension out of range in unsqueeze");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::unsqueeze_(index_type dimension) const {
+    if (dimension < 0 || dimension > static_cast<index_type>(shape_.size()))
+    {
+        throw index_error("Dimension out of range in unsqueeze");
+    }
 
-  this->__shape_.insert(this->__shape_.begin() + __dim, 1);
+    shape_.insert(shape_.begin() + dimension, 1);
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp> tensor<_Tp>::squeeze(index_type __dim) const {
-  __self __ret = this->clone();
-  __ret.squeeze_(__dim);
-  return __ret;
+template<class _Tp>
+tensor<_Tp> tensor<_Tp>::squeeze(index_type dimension) const {
+    self ret = clone();
+    ret.squeeze_(dimension);
+    return ret;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::squeeze_(index_type __dim) {
-  return *this;
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::squeeze_(index_type dimension) {
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::squeeze_(index_type __dim) const {
-  return this->squeeze_(__dim);
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::squeeze_(index_type dimension) const {
+    return squeeze_(dimension);
 }
