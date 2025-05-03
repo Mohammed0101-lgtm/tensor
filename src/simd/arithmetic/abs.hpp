6aa41587e673a191a22ba34f6a56bab49af9b049
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-03 17:54:25 +0100

    stuff

--- src/simd/arithmetic/abs.hpp
+++ src/simd/arithmetic/abs.hpp
@@ -5,7 +5,6 @@
 
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::neon_abs_() {
-    index_type i = 0;
     if (!std::is_arithmetic_v<value_type>)
     {
         throw type_error("Type must be arithmetic");
@@ -16,10 +15,9 @@ tensor<_Tp>& tensor<_Tp>::neon_abs_() {
         return *this;
     }
 
-    constexpr std::size_t simd_width = _ARM64_REG_WIDTH / sizeof(value_type);
-    static_assert(simd_width % 2 == 0, "register width must divide the size of the data type evenly");
     const index_type simd_end = data_.size() - (data_.size() % simd_width);
 
+    index_type i = 0;
     for (; i < simd_end; i += simd_width)
     {
         neon_type<value_type> vec     = neon_load<value_type>(&data_[i]);
