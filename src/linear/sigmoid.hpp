aa916fee0b845bd9c3e518ac9ee20204d76e242f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-04 22:01:10 +0100

    stuff

--- src/linear/sigmoid.hpp
+++ src/linear/sigmoid.hpp
@@ -2,39 +2,39 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp> tensor<_Tp>::sigmoid() const {
-  __self __ret = this->clone();
-  __ret.sigmoid_();
-  return __ret;
+    self ret = clone();
+    ret.sigmoid_();
+    return ret;
 }
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::sigmoid_() {
-#if defined(__ARM_NEON)
-  return this->neon_sigmoid_();
-#endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-  index_type __i = 0;
-  for (; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] =
-        static_cast<value_type>(1.0 / (1.0 + std::exp(-static_cast<double>(this->__data_[__i]))));
+    for (auto& elem : data_)
+    {
+        elem = 1.0 / (1.0 + std::exp(-elem));
+    }
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
+template<class _Tp>
 inline const tensor<_Tp>& tensor<_Tp>::sigmoid_() const {
-#if defined(__ARM_NEON)
-  return this->neon_sigmoid_();
-#endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-  index_type __i = 0;
-  for (; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] =
-        static_cast<value_type>(1.0 / (1.0 + std::exp(-static_cast<double>(this->__data_[__i]))));
+    for (auto& elem : data_)
+    {
+        elem = 1.0 / (1.0 + std::exp(-elem));
+    }
 
-  return *this;
+    return *this;
 }
