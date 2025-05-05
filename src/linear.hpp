de7f64e88a1126ced2c0be70c964bba979db65e6
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-05 20:31:55 +0100

    stuff

--- src/linear.hpp
+++ src/linear.hpp
@@ -3,88 +3,94 @@
 #include "tensorbase.hpp"
 #include "types.hpp"
 
-template <class _Tp>
-tensor<_Tp> tensor<_Tp>::reshape(const shape_type __sh) const {
-  data_t     __d = this->__data_;
-  index_type __s = this->__computeSize(__sh);
+template<class _Tp>
+tensor<_Tp> tensor<_Tp>::reshape(const shape_type shape_) const {
+    data_t     d = data_;
+    index_type s = computeSize(shape_);
 
-  if (__s != this->__data_.size())
-    throw __shape_error__(
-        "input shape must have size of elements equal to the current number of elements in the "
-        "tensor data");
+    if (s != data_.size())
+        throw shape_error("input shape must have size of elements equal to the current number of elements in the "
+                          "tensor data");
 
-  return __self(__d, __sh);
+    return self(shape_, d);
 }
 
-template <class _Tp>
-tensor<_Tp> tensor<_Tp>::absolute(const tensor& __tensor) const {
-  index_type __s = __tensor.storage().size();
-  data_t     __a;
-  __a.reserve(__s);
-  index_type __i = 0;
-  for (; __i < __s; ++__i)
-    __a.push_back(static_cast<value_type>(std::fabs(_f32(__tensor.storage()[__i]))));
+template<class _Tp>
+tensor<_Tp> tensor<_Tp>::absolute(const tensor& other) const {
+    index_type s = other.storage().size();
+    data_t     a(s);
 
-  return __self(__a, __tensor.__shape_);
+    index_type i = 0;
+    for (; i < s; ++i)
+        a[i] = static_cast<value_type>(std::fabs(static_cast<_f32>(other[i])));
+
+    return self(other.shape(), a);
 }
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::pop_back() const {
-  if (__equal_shape(this->__shape_, shape_type(1, this->__shape_[0])))
-    throw __index_error__("push_back is only supported for one dimensional tensors");
+    if (equal_shape(shape_, shape_type(1, shape_[0])))
+        throw index_error("push_back is only supported for one dimensional tensors");
 
-  this->__data_.pop_back();
-  --(this->__shape_[0]);
-  this->__compute_strides();
-  return *this;
+    data_.pop_back();
+    --(shape_[0]);
+    compute_strides();
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp> tensor<_Tp>::cat(const std::vector<tensor<_Tp>>& __others, index_type __dim) const {
-  for (const tensor& __t : __others) {
-    index_type __i = 0;
-    for (; __i < this->__shape_.size(); ++__i)
-      if (__i != __dim && this->__shape_[__i] != __t.__shape_[__i])
-        throw __shape_error__(
-            "Cannot concatenate tensors with different shapes along non-concatenation "
-            "dimensions");
-  }
+template<class _Tp>
+tensor<_Tp> tensor<_Tp>::cat(const std::vector<tensor<_Tp>>& others, index_type dimension) const {
+    for (const tensor& t : others)
+    {
+        index_type i = 0;
+        for (; i < shape_.size(); ++i)
+            if (i != dimension && shape_[i] != t.shape_[i])
+                throw shape_error("Cannot concatenate tensors with different shapes along non-concatenation "
+                                  "dimensions");
+    }
 
-  shape_type __ret_sh = this->__shape_;
-  for (const tensor& __t : __others) __ret_sh[__dim] += __t.__shape_[__dim];
+    shape_type ret_sh = shape_;
+    for (const tensor& t : others)
+        ret_sh[dimension] += t.shape_[dimension];
 
-  data_t __c;
-  __c.reserve(this->__data_.size());
-  __c.insert(__c.end(), this->__data_.begin(), this->__data_.end());
-  for (const tensor& __t : __others) __c.insert(__c.end(), __t.__data_.begin(), __t.__data_.end());
+    data_t c;
+    c.reserve(data_.size());
+    c.insert(c.end(), data_.begin(), data_.end());
+    for (const tensor& t : others)
+        c.insert(c.end(), t.data_.begin(), t.data_.end());
 
-  return __self(__ret_sh, __c);
+    return self(ret_sh, c);
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::log_softmax_(const index_type __dim) {
-  assert(__dim < this->__shape_.size() && "Dimension out of range for log_softmax");
-  tensor __max_values  = this->argmax_(__dim);
-  tensor __shifted     = *this - __max_values.expand_as(this->__shape_, __dim);
-  tensor __exp_values  = __shifted.exp();
-  tensor __sum_exp     = __exp_values.sum(__dim);
-  tensor __log_sum_exp = __sum_exp.log();
-  *this                = __shifted - __log_sum_exp.expand_as(this->__shape_, __dim);
-  return *this;
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::log_softmax_(const index_type dimension) {
+    if (dimension < shape_.size())
+        throw std::invalid_argument("Dimension out of range for log_softmax");
+    tensor max_values  = argmax_(dimension);
+    tensor shifted     = *this - max_values.expand_as(shape_, dimension);
+    tensor exp_values  = shifted.exp();
+    tensor sum_exp     = exp_values.sum(dimension);
+    tensor log_sum_exp = sum_exp.log();
+    *this              = shifted - log_sum_exp.expand_as(shape_, dimension);
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::log_softmax_(const index_type __dim) const {
-  return this->log_softmax_(__dim);
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::log_softmax_(const index_type dimension) const {
+    return log_softmax_(dimension);
 }
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::resize_as_(const shape_type __sh) {
-  // TODO: implement in place resize as here
-  return *this;
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::resize_as_(const shape_type shape_) {
+    // TODO: implement in place resize as here
+    shape_ = shape_;
+    compute_strides();
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::resize_as_(const shape_type __sh) const {
-  return this->resize_as_(__sh);
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::resize_as_(const shape_type shape_) const {
+    shape_ = shape_;
+    compute_strides();
+    return *this;
 }
