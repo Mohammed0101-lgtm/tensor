a534f4a5e1f91d39cd3ab2eca7fb9888d9f931e3
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-05 20:33:32 +0100

    stuff

--- src/bit.hpp
+++ src/bit.hpp
@@ -2,350 +2,320 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::bitwise_right_shift_(const int __amount) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_right_shift_(__amount);
-#endif
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::bitwise_right_shift_(const int amount) {
+    if constexpr (!std::is_integral_v<value_type>)
+        throw type_error("Type must be integral");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] >>= __amount;
+    for (auto& elem : data_)
+        elem >>= amount;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_right_shift_(const int __amount) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_right_shift_(__amount);
-#endif
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_right_shift_(const int amount) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Type must be integral");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] >>= __amount;
+    for (auto& elem : data_)
+        elem >>= amount;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::bitwise_left_shift_(const int __amount) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_left_shift_(__amount);
-#endif
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::bitwise_left_shift_(const int amount) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Type must be integral");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] <<= __amount;
+    for (auto& elem : data_)
+        elem <<= amount;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_left_shift_(const int __amount) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_left_shift_(__amount);
-#endif
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_left_shift_(const int amount) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Type must be integral");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] <<= __amount;
+    for (auto& elem : data_)
+        elem <<= amount;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::bitwise_or_(const value_type __val) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_or_(__val);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise OR on non-integral values");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::bitwise_or_(const value_type value) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise OR on non-integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] |= __val;
+    for (auto& elem : data_)
+        elem |= value;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_or_(const value_type __val) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_or_(__val);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise OR on non-integral values");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_or_(const value_type value) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise OR on non-integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] |= __val;
+    for (auto& elem : data_)
+        elem |= value;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const value_type __val) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_xor_(__val);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise XOR on non-integral values");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const value_type value) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise XOR on non-integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] ^= __val;
+    for (auto& elem : data_)
+        elem ^= value;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const value_type __val) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_xor_(__val);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise XOR on non-integral");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const value_type value) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise XOR on non-integral");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] ^= __val;
+    for (auto& elem : data_)
+        elem ^= value;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_xor(const value_type __val) const {
-  __self __ret = this->clone();
-  __ret.bitwise_xor_(__val);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_xor(const value_type value) const {
+    self ret = clone();
+    ret.bitwise_xor_(value);
+    return ret;
 }
 
-template <class _Tp>
+template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::bitwise_not() const {
-  __self __ret = this->clone();
-  __ret.bitwise_not_();
-  return __ret;
+    self ret = clone();
+    ret.bitwise_not_();
+    return ret;
 }
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::bitwise_not_() {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_not_();
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise NOT on non integral values");
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise NOT on non integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = ~this->__data_[__i];
+    for (auto& elem : data_)
+        elem = ~elem;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
+template<class _Tp>
 inline const tensor<_Tp>& tensor<_Tp>::bitwise_not_() const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_not_();
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise NOT on non integral values");
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise NOT on non integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = ~this->__data_[__i];
+    for (auto& elem : data_)
+        elem = ~elem;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::bitwise_and_(const value_type __val) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_and_(__val);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise AND on non-integral values");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::bitwise_and_(const value_type value) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise AND on non-integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] &= __val;
+    for (auto& elem : data_)
+        elem &= value;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_and_(const value_type __val) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_and_(__val);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise AND on non-integral values");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_and_(const value_type value) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise AND on non-integral values");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] &= __val;
+    for (auto& elem : data_)
+        elem &= value;
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_and(const value_type __val) const {
-  __self __ret = this->clone();
-  __ret.bitwise_and_(__val);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_and(const value_type value) const {
+    self ret = clone();
+    ret.bitwise_and_(value);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_and(const tensor& __other) const {
-  __self __ret = this->clone();
-  __ret.bitwise_and_(__other);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_and(const tensor& other) const {
+    self ret = clone();
+    ret.bitwise_and_(other);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_left_shift(const int __amount) const {
-  __self __ret = this->clone();
-  __ret.bitwise_left_shift_(__amount);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_left_shift(const int amount) const {
+    self ret = clone();
+    ret.bitwise_left_shift_(amount);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_xor(const tensor& __other) const {
-  __self __ret = this->clone();
-  __ret.bitwise_xor_(__other);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_xor(const tensor& other) const {
+    self ret = clone();
+    ret.bitwise_xor_(other);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_right_shift(const int __amount) const {
-  __self __ret = this->clone();
-  __ret.bitwise_right_shift_(__amount);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_right_shift(const int amount) const {
+    self ret = clone();
+    ret.bitwise_right_shift_(amount);
+    return ret;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::bitwise_and_(const tensor& __other) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_and_(__other);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise AND on non-integral values");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::bitwise_and_(const tensor& other) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise AND on non-integral values");
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensor shapes must be equal");
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] &= __other[__i];
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensor shapes must be equal");
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem &= other[i++];
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_and_(const tensor& __other) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_and_(__other);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise AND on non-integral values");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_and_(const tensor& other) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise AND on non-integral values");
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] &= __other[__i];
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensors shapes must be equal");
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem &= other[i++];
+
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_or(const tensor& __other) const {
-  __self __ret = this->clone();
-  __ret.bitwise_or_(__other);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_or(const tensor& other) const {
+    self ret = clone();
+    ret.bitwise_or_(other);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::bitwise_or(const value_type __val) const {
-  __self __ret = this->clone();
-  __ret.bitwise_or_(__val);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::bitwise_or(const value_type value) const {
+    self ret = clone();
+    ret.bitwise_or_(value);
+    return ret;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::bitwise_or_(const tensor& __other) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_or_(__other);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise OR on non-integral values");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::bitwise_or_(const tensor& other) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise OR on non-integral values");
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] |= __other[__i];
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensors shapes must be equal");
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem |= other[i++];
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_or_(const tensor& __other) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_or_(__other);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise OR on non-integral values");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_or_(const tensor& other) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise OR on non-integral values");
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] |= __other[__i];
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensors shapes must be equal");
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem |= other[i++];
+
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const tensor& __other) {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_xor_(__other);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise XOR on non-integral values");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const tensor& other) {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise XOR on non-integral values");
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] ^= __other[__i];
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensors shapes must be equal");
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem ^= other[i++];
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const tensor& __other) const {
-#if defined(__ARM_NEON)
-  return this->neon_bitwise_xor_(__other);
-#endif
-  if (!std::is_integral_v<value_type>)
-    throw __type_error__("Cannot perform a bitwise XOR on non-integral values");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::bitwise_xor_(const tensor& other) const {
+    if (!std::is_integral_v<value_type>)
+        throw type_error("Cannot perform a bitwise XOR on non-integral values");
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] ^= __other[__i];
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensors shapes must be equal");
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem ^= other[i++];
+
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::fill_(const value_type __val) {
-  this->__data_(this->__data_.size(), __val);
-  return *this;
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::fill_(const value_type value) {
+    for (auto& elem : data_)
+        elem = value;
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::fill_(const value_type __val) const {
-  return this->fill_(__val);
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::fill_(const value_type value) const {
+    for (auto& elem : data_)
+        elem = value;
+
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::fill_(const tensor& __other) {
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::fill_(const tensor& other) {
+    if (!equal_shape(shape(), other.shape()))
+        throw shape_error("Tensors shapes must be equal");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] = __other[__i];
+    index_type i = 0;
+    for (auto& elem : data_)
+        elem = other[i++];
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::fill_(const tensor& __other) const {
-  return this->fill_(__other);
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::fill_(const tensor& other) const {
+    return fill_(other);
 }
