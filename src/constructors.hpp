2c38de4b52a654d579cea1f06ed1778d3216d9f2
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-05 20:33:04 +0100

    stuff

--- src/constructors.hpp
+++ src/constructors.hpp
@@ -2,55 +2,65 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
-tensor<_Tp>::tensor(const shape_type& __sh, const_reference __v, Device __d)
-    : __shape_(__sh), __data_(this->__computeSize(__sh), __v), __device_(__d) {
-  this->__compute_strides();
+template<class _Tp>
+tensor<_Tp>::tensor(const shape_type& shape_, const_reference v, Device d) :
+    shape_(shape_),
+    data_(computeSize(shape_), v),
+    device_(d) {
+    compute_strides();
 }
 
-template <class _Tp>
-tensor<_Tp>::tensor(const shape_type& __sh, Device __d) : __shape_(__sh), __device_(__d) {
-  index_type __s = this->__computeSize(__sh);
-  this->__data_  = data_t(__s);
-  this->__compute_strides();
+template<class _Tp>
+tensor<_Tp>::tensor(const shape_type& shape_, Device d) :
+    shape_(shape_),
+    device_(d) {
+    index_type s = computeSize(shape_);
+    data_        = data_t(s);
+    compute_strides();
 }
 
-template <class _Tp>
-tensor<_Tp>::tensor(const shape_type& __sh, const data_t& __d, Device __dev)
-    : __shape_(__sh), __device_(__dev) {
-  index_type __s = this->__computeSize(__sh);
-  assert(__d.size() == static_cast<size_t>(__s) &&
-         "Initial data vector must match the tensor size");
-  this->__data_ = __d;
-  this->__compute_strides();
+template<class _Tp>
+tensor<_Tp>::tensor(const shape_type& shape_, const data_t& d, Device dev) :
+    shape_(shape_),
+    device_(dev) {
+    index_type s = computeSize(shape_);
+    
+    if (d.size() != static_cast<std::size_t>(s))
+        throw std::invalid_argument("Initial data vector must match the tensor size");
+
+    data_ = d;
+    compute_strides();
 }
 
-template <class _Tp>
-tensor<_Tp>::tensor(const shape_type& __sh, std::initializer_list<value_type> init_list, Device __d)
-    : __shape_(__sh), __device_(__d) {
-  if (static_cast<index_type>(init_list.size()) != this->__computeSize(__sh))
-    throw std::invalid_argument("Initializer list size must match the tensor size");
+template<class _Tp>
+tensor<_Tp>::tensor(const shape_type& shape_, std::initializer_list<value_type> init_list, Device d) :
+    shape_(shape_),
+    device_(d) {
+    if (static_cast<index_type>(init_list.size()) != computeSize(shape_))
+        throw std::invalid_argument("Initializer list size must match the tensor size");
 
-  this->__data_ = data_t(init_list);
-  this->__compute_strides();
+    data_ = data_t(init_list);
+    compute_strides();
 }
 
-template <class _Tp>
-tensor<_Tp>::tensor(const shape_type& __sh, const tensor& __other)
-    : __data_(__other.storage()), __shape_(__sh), __device_(__other.device()) {
-  this->__compute_strides();
+template<class _Tp>
+tensor<_Tp>::tensor(const shape_type& shape_, const tensor& other) :
+    data_(other.storage()),
+    shape_(shape_),
+    device_(other.device()) {
+    compute_strides();
 }
 
-template <class _Tp>
-tensor<_Tp>::tensor(const tensor& __t)
-    : __data_(__t.storage()),
-      __shape_(__t.shape()),
-      __strides_(__t.strides()),
-      __device_(__t.device()) {}
+template<class _Tp>
+tensor<_Tp>::tensor(const tensor& t) :
+    data_(t.storage()),
+    shape_(t.shape()),
+    strides_(t.strides()),
+    device_(t.device()) {}
 
-template <class _Tp>
-tensor<_Tp>::tensor(tensor&& __t) noexcept
-    : __data_(std::move(__t.storage())),
-      __shape_(std::move(__t.shape())),
-      __strides_(std::move(__t.strides())),
-      __device_(std::move(__t.device())) {}
+template<class _Tp>
+tensor<_Tp>::tensor(tensor&& t) noexcept :
+    data_(std::move(t.storage())),
+    shape_(std::move(t.shape())),
+    strides_(std::move(t.strides())),
+    device_(std::move(t.device())) {}
