915f7b70d0c19a1c5720afb9a08eddd8d353899d
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-07 19:02:11 +0100

    stuff

--- src/arithmetic/rem.hpp
+++ src/arithmetic/rem.hpp
@@ -2,58 +2,102 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::remainder(const value_type __val) const {
-  __self __ret = this->clone();
-  __ret.remainder_(__val);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::remainder(const value_type value) const {
+    self ret = clone();
+    ret.remainder_(value);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::remainder(const tensor& __other) const {
-  __self __ret = this->clone();
-  __ret.remainder_(__other);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::remainder(const tensor& other) const {
+    self ret = clone();
+    ret.remainder_(other);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::remainder_(const value_type __val) {
-  assert(__val != 0 && "Remainder by zero is undefined");
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::remainder_(const value_type value) {
+    if constexpr (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithemtic");
+    }
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] %= __val;
-  return *this;
+    if (!value)
+    {
+        throw std::invalid_argument("Remainder by zero is undefined");
+    }
+
+    for (auto& elem : data_)
+    {
+        elem %= value;
+    }
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::remainder_(const value_type __val) const {
-  assert(__val != 0 && "Remainder by zero is undefined");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::remainder_(const value_type value) const {
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arihtmetic");
+    }
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] %= __val;
-  return *this;
+    if (!value)
+    {
+        throw std::invalid_argument("Remainder by zero is undefined");
+    }
+
+    for (auto& elem : data_)
+    {
+        elem %= value;
+    }
+
+    return *this;
 }
 
-template <class _Tp>
-inline tensor<_Tp>& tensor<_Tp>::remainder_(const tensor& __other) {
-  assert(__other.count_nonzero() == __other.size(0) && "Remainder by zero is undefined");
+template<class _Tp>
+inline tensor<_Tp>& tensor<_Tp>::remainder_(const tensor& other) {
+    if constexpr (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
+    assert(other.count_nonzero() == other.size(0) && "Remainder by zero is undefined");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] %= __other[__i];
-  return *this;
+    if (!equal_shape(shape(), other.shape()))
+    {
+        throw shape_error("Tensors shapes must be equal");
+    }
+
+    index_type i = 0;
+    for (auto& elem : data_)
+    {
+        elem %= other[i++];
+    }
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::remainder_(const tensor& __other) const {
-  assert(__other.count_nonzero() == __other.size(0) && "Remainder by zero is undefined");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::remainder_(const tensor& other) const {
+    if constexpr (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
+    assert(other.count_nonzero() == other.size(0) && "Remainder by zero is undefined");
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i) this->__data_[__i] %= __other[__i];
-  return *this;
+    if (!equal_shape(shape(), other.shape()))
+    {
+        throw shape_error("Tensors shapes must be equal");
+    }
+
+    index_type i = 0;
+    for (auto& elem : data_)
+    {
+        elem %= other[i++];
+    }
+
+    return *this;
 }
