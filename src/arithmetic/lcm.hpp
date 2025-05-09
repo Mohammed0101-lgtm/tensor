4d2345bc5c955ea89539a555d747a13f16d635e0
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-09 14:17:13 +0100

    stuff

--- src/arithmetic/lcm.hpp
+++ src/arithmetic/lcm.hpp
@@ -3,33 +3,39 @@
 #include "tensorbase.hpp"
 
 // used as a helper function
-inline int64_t __lcm(const int64_t __a, const int64_t __b) {
-  return (__a * __b) / std::gcd(__a, __b);
-}
+inline int64_t __lcm(const int64_t a, const int64_t b) { return (a * b) / std::gcd(a, b); }
 
-template <class _Tp>
+template<class _Tp>
 inline typename tensor<_Tp>::index_type tensor<_Tp>::lcm() const {
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-  index_type __ret = static_cast<index_type>(this->__data_[0]);
+    index_type ret = static_cast<index_type>(data_[0]);
 
-  for (index_type __i = 1; __i < this->__data_.size(); ++__i)
-    __ret = __lcm(static_cast<index_type>(this->__data_[__i]), __ret);
+    for (const auto& elem : data_)
+    {
+        ret = __lcm(elem, ret);
+    }
 
-  return __ret;
+    return ret;
 }
 
-template <class _Tp>
-tensor<_Tp> tensor<_Tp>::lcm(const tensor& __other) const {
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
+template<class _Tp>
+tensor<_Tp> tensor<_Tp>::lcm(const tensor& other) const {
+    if (!equal_shape(shape(), other.shape()))
+    {
+        throw shape_error("Tensors shapes must be equal");
+    }
 
-  tensor __ret = this->clone();
+    tensor ret = clone();
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    __ret[__i] = static_cast<value_type>(this->__lcm(static_cast<index_type>(this->__data_[__i]),
-                                                     static_cast<index_type>(__other[__i])));
+    index_type i = 0;
+    for (const auto& elem : data_)
+    {
+        ret[i++] = lcm(elem, other[i]);
+    }
 
-  return __ret;
+    return ret;
 }
