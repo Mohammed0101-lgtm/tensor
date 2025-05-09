ba03d358a08b98e5f8dc75e0e6aa7a359d716c2f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-09 14:17:46 +0100

    stuff

--- src/arithmetic/dist.hpp
+++ src/arithmetic/dist.hpp
@@ -1,80 +1,111 @@
 #pragma once
 
 #include "tensorbase.hpp"
+#include "types.hpp"
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::dist(const tensor& __other) const {
-  __self __ret = this->clone();
-  __ret.dist_(__other);
-  return __ret;
+
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::dist(const tensor& other) const {
+    self ret = clone();
+    ret.dist_(other);
+    return ret;
 }
 
-template <class _Tp>
-inline tensor<_Tp> tensor<_Tp>::dist(const value_type __val) const {
-  __self __ret = this->clone();
-  __ret.dist_(__val);
-  return __ret;
+template<class _Tp>
+inline tensor<_Tp> tensor<_Tp>::dist(const value_type value) const {
+    self ret = clone();
+    ret.dist_(value);
+    return ret;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::dist_(const tensor& __other) {
-#if defined(__ARM_NEON)
-  return this->neon_dist_(__other);
-#endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::dist_(const tensor& other) {
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-  if (!__equal_shape(this->shape(), __other.shape()))
-    throw __shape_error__("Tensors shapes must be equal");
+    if (!equal_shape(shape(), other.shape()))
+    {
+        throw shape_error("Tensors shapes must be equal");
+    }
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] =
-        static_cast<value_type>(std::abs(static_cast<_f64>(this->__data_[__i] - __other[__i])));
+    if constexpr (!has_minus_operator_v<value_type>)
+    {
+        throw operator_error("Value type must have a minus operator");
+    }
 
-  return *this;
+    index_type i = 0;
+    for (auto& elem : data_)
+    {
+        elem = std::abs(elem - other[i++]);
+    }
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::dist_(const tensor& __other) const {
-#if defined(__ARM_NEON)
-  return this->neon_dist_(__other);
-#endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::dist_(const tensor& other) const {
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] =
-        static_cast<value_type>(std::abs(static_cast<_f64>(this->__data_[__i] - __other[__i])));
+    if (!equal_shape(shape(), other.shape()))
+    {
+        throw shape_error("Tensors shapes must be equal");
+    }
 
-  return *this;
+    if constexpr (!has_minus_operator_v<value_type>)
+    {
+        throw operator_error("Value type must have a minus operator");
+    }
+
+    index_type i = 0;
+    for (auto& elem : data_)
+    {
+        elem = std::abs(data_[i] - other[i++]);
+    }
+
+    return *this;
 }
 
-template <class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::dist_(const value_type __val) const {
-#if defined(__ARM_NEON)
-  return this->neon_dist_(__val);
-#endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+template<class _Tp>
+inline const tensor<_Tp>& tensor<_Tp>::dist_(const value_type value) const {
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] =
-        static_cast<value_type>(std::abs(static_cast<_f64>(this->__data_[__i] - __val)));
+    if constexpr (!has_minus_operator_v<value_type>)
+    {
+        throw operator_error("Value type must have a minus operator");
+    }
 
-  return *this;
+    for (auto& elem : data_)
+    {
+        elem = std::abs(elem - value);
+    }
+
+    return *this;
 }
 
-template <class _Tp>
-tensor<_Tp>& tensor<_Tp>::dist_(const value_type __val) {
-#if defined(__ARM_NEON)
-  return this->neon_dist_(__val);
-#endif
-  if (!std::is_arithmetic_v<value_type>) throw __type_error__("Type must be arithmetic");
+template<class _Tp>
+tensor<_Tp>& tensor<_Tp>::dist_(const value_type value) {
+    if (!std::is_arithmetic_v<value_type>)
+    {
+        throw type_error("Type must be arithmetic");
+    }
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] =
-        static_cast<value_type>(std::abs(static_cast<_f64>(this->__data_[__i] - __val)));
+    if constexpr (!has_minus_operator_v<value_type>)
+    {
+        throw operator_error("Value type must have a minus operator");
+    }
 
-  return *this;
+    for (auto& elem : data_)
+    {
+        elem = std::abs(elem - value);
+    }
+
+    return *this;
 }
