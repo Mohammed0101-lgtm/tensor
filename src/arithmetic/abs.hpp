d80f6981c0de85c1521060ae21d342a9c665eec8
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-16 14:05:18 +0100

    stuff

--- src/arithmetic/abs.hpp
+++ src/arithmetic/abs.hpp
@@ -2,43 +2,43 @@
 
 #include "tensorbase.hpp"
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp> tensor<_Tp>::abs() const {
-  __self __ret = this->clone();
-  __ret.abs_();
-  return __ret;
+    self ret = clone();
+    ret.abs_();
+    return ret;
 }
 
-template <class _Tp>
+template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::abs_() {
 #if defined(__ARM_NEON)
-  return this->neon_abs_();
+    return neon_abs_();
 #endif
-  if (!std::is_arithmetic_v<value_type>)
-    throw __type_error__("abs_() is only available for arithmetic types.");
+    if (!std::is_arithmetic_v<value_type>)
+        throw type_error("abs_() is only available for arithmetic types.");
 
-  if (std::is_unsigned_v<value_type>) return *this;
+    if (std::is_unsigned_v<value_type>)
+        return *this;
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::abs(this->__data_[__i]));
+    for (auto& elem : data_)
+        elem = std::abs(elem);
 
-  return *this;
+    return *this;
 }
 
-template <class _Tp>
+template<class _Tp>
 inline const tensor<_Tp>& tensor<_Tp>::abs_() const {
 #if defined(__ARM_NEON)
-  return this->neon_abs_();
+    return neon_abs_();
 #endif
-  if (!std::is_arithmetic_v<value_type>)
-    throw __type_error__("abs_() is only available for arithmetic types.");
+    if (!std::is_arithmetic_v<value_type>)
+        throw type_error("abs_() is only available for arithmetic types.");
 
-  if (std::is_unsigned_v<value_type>) return *this;
+    if (std::is_unsigned_v<value_type>)
+        return *this;
 
-#pragma omp parallel
-  for (index_type __i = 0; __i < this->__data_.size(); ++__i)
-    this->__data_[__i] = static_cast<value_type>(std::abs(this->__data_[__i]));
+    for (auto& elem : data_)
+        elem = std::abs(elem);
 
-  return *this;
+    return *this;
 }
