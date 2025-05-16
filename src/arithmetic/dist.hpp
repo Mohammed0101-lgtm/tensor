4f810f9c0e7f3fee5ef1d0d730394693346b5f50
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-16 14:04:03 +0100

    stuff

--- src/arithmetic/dist.hpp
+++ src/arithmetic/dist.hpp
@@ -21,25 +21,17 @@ inline tensor<_Tp> tensor<_Tp>::dist(const value_type value) const {
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::dist_(const tensor& other) {
     if (!std::is_arithmetic_v<value_type>)
-    {
         throw type_error("Type must be arithmetic");
-    }
 
     if (!equal_shape(shape(), other.shape()))
-    {
         throw shape_error("Tensors shapes must be equal");
-    }
 
     if constexpr (!has_minus_operator_v<value_type>)
-    {
         throw operator_error("Value type must have a minus operator");
-    }
 
     index_type i = 0;
     for (auto& elem : data_)
-    {
         elem = std::abs(elem - other[i++]);
-    }
 
     return *this;
 }
@@ -47,25 +39,17 @@ tensor<_Tp>& tensor<_Tp>::dist_(const tensor& other) {
 template<class _Tp>
 inline const tensor<_Tp>& tensor<_Tp>::dist_(const tensor& other) const {
     if (!std::is_arithmetic_v<value_type>)
-    {
         throw type_error("Type must be arithmetic");
-    }
 
     if (!equal_shape(shape(), other.shape()))
-    {
         throw shape_error("Tensors shapes must be equal");
-    }
 
     if constexpr (!has_minus_operator_v<value_type>)
-    {
         throw operator_error("Value type must have a minus operator");
-    }
 
     index_type i = 0;
     for (auto& elem : data_)
-    {
         elem = std::abs(data_[i] - other[i++]);
-    }
 
     return *this;
 }
@@ -73,19 +57,13 @@ inline const tensor<_Tp>& tensor<_Tp>::dist_(const tensor& other) const {
 template<class _Tp>
 inline const tensor<_Tp>& tensor<_Tp>::dist_(const value_type value) const {
     if (!std::is_arithmetic_v<value_type>)
-    {
         throw type_error("Type must be arithmetic");
-    }
 
     if constexpr (!has_minus_operator_v<value_type>)
-    {
         throw operator_error("Value type must have a minus operator");
-    }
 
     for (auto& elem : data_)
-    {
         elem = std::abs(elem - value);
-    }
 
     return *this;
 }
@@ -93,19 +71,13 @@ inline const tensor<_Tp>& tensor<_Tp>::dist_(const value_type value) const {
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::dist_(const value_type value) {
     if (!std::is_arithmetic_v<value_type>)
-    {
         throw type_error("Type must be arithmetic");
-    }
 
     if constexpr (!has_minus_operator_v<value_type>)
-    {
         throw operator_error("Value type must have a minus operator");
-    }
 
     for (auto& elem : data_)
-    {
         elem = std::abs(elem - value);
-    }
 
     return *this;
 }
