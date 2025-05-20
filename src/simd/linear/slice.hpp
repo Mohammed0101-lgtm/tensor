f7063aa06b6bb91b3016556a2fd6b0246705f981
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-20 23:29:22 +0100

    stuff

--- src/simd/linear/slice.hpp
+++ src/simd/linear/slice.hpp
@@ -4,13 +4,13 @@
 
 /*
 template<class _Tp>
-tensor<_Tp> tensor<_Tp>::neon_slice(index_type                dimension,
-                                    std::optional<index_type> start,
-                                    std::optional<index_type> end,
-                                    index_type                step) const {
-    if (dimension < 0 or dimension >= static_cast<index_type>(shape_.size()))
+tensor<_Tp> tensor<_Tp>::neon_slice(_u64                dimension,
+                                    std::optional<_u64> start,
+                                    std::optional<_u64> end,
+                                    _u64                step) const {
+    if (dimension < 0 or dimension >= static_cast<_u64>(shape_.size()))
     {
-        throw index_error("Dimension out of range.");
+        throw error::index_error("Dimension out of range.");
     }
 
     if (step == 0)
@@ -18,9 +18,9 @@ tensor<_Tp> tensor<_Tp>::neon_slice(index_type                dimension,
         throw std::invalid_argument("Step cannot be zero.");
     }
 
-    index_type s       = shape_[dimension];
-    index_type start_i = start.value_or(0);
-    index_type end_i   = end.value_or(s);
+    _u64 s       = shape_[dimension];
+    _u64 start_i = start.value_or(0);
+    _u64 end_i   = end.value_or(s);
 
     if (start_i < 0)
     {
@@ -32,34 +32,34 @@ tensor<_Tp> tensor<_Tp>::neon_slice(index_type                dimension,
         end_i += s;
     }
 
-    start_i               = std::max(index_type(0), std::min(start_i, s));
-    end_i                 = std::max(index_type(0), std::min(end_i, s));
-    index_type slice_size = (end_i - start_i + step - 1) / step;
+    start_i               = std::max(_u64(0), std::min(start_i, s));
+    end_i                 = std::max(_u64(0), std::min(end_i, s));
+    _u64 slice_size = (end_i - start_i + step - 1) / step;
     shape_type ret_dims   = shape_;
     ret_dims[dimension]         = slice_size;
     tensor ret(ret_dims);
 
-    index_type vector_end = start_i + ((end_i - start_i) / _ARM64_REG_WIDTH) * _ARM64_REG_WIDTH;
+    _u64 vector_end = start_i + ((end_i - start_i) / _ARM64_REG_WIDTH) * _ARM64_REG_WIDTH;
 
-    if constexpr (std::is_floating_point_v<value_type> and step == 1)
+    if constexpr (std::is_floating_point_v<_Tp> and step == 1)
     {
-        for (index_type i = start_i, j = 0; i < vector_end; i += _ARM64_REG_WIDTH, j += _ARM64_REG_WIDTH)
+        for (_u64 i = start_i, j = 0; i < vector_end; i += _ARM64_REG_WIDTH, j += _ARM64_REG_WIDTH)
         {
             neon_f32 vec = vld1q_f32(reinterpret_cast<const _f32*>(&data_[i]));
             vst1q_f32(&(ret.data_[j]), vec);
         }
     }
-    else if (std::is_signed_v<value_type> and step == 1)
+    else if (std::is_signed_v<_Tp> and step == 1)
     {
-        for (index_type i = start_i, j = 0; i < vector_end; i += _ARM64_REG_WIDTH, j += _ARM64_REG_WIDTH)
+        for (_u64 i = start_i, j = 0; i < vector_end; i += _ARM64_REG_WIDTH, j += _ARM64_REG_WIDTH)
         {
             neon_s32 vec = vld1q_s32(reinterpret_cast<const _s32*>(&data_[i]));
             vst1q_s32(&(ret.data_[j]), vec);
         }
     }
-    else if constexpr (std::is_unsigned_v<value_type> and step == 1)
+    else if constexpr (std::is_unsigned_v<_Tp> and step == 1)
     {
-        for (index_type i = start_i, j = 0; i < vector_end; i += _ARM64_REG_WIDTH, j += _ARM64_REG_WIDTH)
+        for (_u64 i = start_i, j = 0; i < vector_end; i += _ARM64_REG_WIDTH, j += _ARM64_REG_WIDTH)
         {
             neon_u32 vec = vld1q_u32(reinterpret_cast<const _u32*>(&data_[i]));
             vst1q_u32(&(ret.data_[j]), vec);
@@ -67,18 +67,18 @@ tensor<_Tp> tensor<_Tp>::neon_slice(index_type                dimension,
     }
 
     // Handle remaining elements
-    index_type remaining = (end_i - start_i) % _ARM64_REG_WIDTH;
+    _u64 remaining = (end_i - start_i) % _ARM64_REG_WIDTH;
     if (remaining > 0)
     {
-        for (index_type i = vector_end, j = vector_end - start_i; i < end_i; ++i, ++j)
+        for (_u64 i = vector_end, j = vector_end - start_i; i < end_i; ++i, ++j)
         {
             ret.data_[j] = data_[i];
         }
     }
 
-    for (index_type i = start_i; i < end_i; i += step)
+    for (_u64 i = start_i; i < end_i; i += step)
     {
-        index_type j = (i - start_i) / step;
+        _u64 j = (i - start_i) / step;
         ret[j]       = data_[i];
     }
 
