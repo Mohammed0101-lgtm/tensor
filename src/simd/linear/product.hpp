94ada109ed869d1247de6a66e75e653eb6d1ce82
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-21 13:26:13 +0100

    stuff

--- src/simd/linear/product.hpp
+++ src/simd/linear/product.hpp
@@ -3,71 +3,56 @@
 #include "tensorbase.hpp"
 
 template<class _Tp>
-tensor<_Tp> tensor<_Tp>::neon_cross_product(const tensor& other) const {
-    if (!std::is_arithmetic_v<value_type>)
-    {
-        throw type_error("Type must be arithmetic");
-    }
+tensor<_Tp> internal::neon::cross_product(tensor<_Tp>& t, const tensor<_Tp>& other) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
 
     if (empty() || other.empty())
-    {
         throw std::invalid_argument("Cannot cross product an empty vector");
-    }
 
     if (!equal_shape(shape(), shape_type({3})) || !equal_shape(other.shape(), shape_type({3})))
-    {
-        throw shape_error("Cross product can only be performed on 3-element vectors");
-    }
+        throw error::shape_error("Cross product can only be performed on 3-element vectors");
 
-    tensor ret({3});
+    tensor            ret({3});
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
 
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
+    neon_type<_Tp> a      = neon_load<_Tp>(data_.data());
+    neon_type<_Tp> b      = neon_load<_Tp>(other.storage().data());
+    neon_type<_Tp> a_yzx  = neon_ext<_Tp>(a, a, 1);
+    neon_type<_Tp> b_yzx  = neon_ext<_Tp>(b, b, 1);
+    neon_type<_Tp> result = neon_sub<_Tp>(neon_mul<_Tp>(a_yzx, b), neon_mul<_Tp>(a, b_yzx));
+    result                = neon_ext(result, result, 3);
 
-    neon_type<value_type> a      = neon_load<value_type>(data_.data());
-    neon_type<value_type> b      = neon_load<value_type>(other.storage().data());
-    neon_type<value_type> a_yzx  = neon_ext<value_type>(a, a, 1);
-    neon_type<value_type> b_yzx  = neon_ext<value_type>(b, b, 1);
-    neon_type<value_type> result = neon_sub<value_type>(neon_mul<value_type>(a_yzx, b), neon_mul<value_type>(a, b_yzx));
-    result                       = neon_ext(result, result, 3);
-
-    neon_store<value_type>(ret.storage().data(), result);
+    neon_store<_Tp>(ret.storage().data(), result);
 
     return ret;
 }
 
 template<class _Tp>
-tensor<_Tp> tensor<_Tp>::neon_dot(const tensor& other) const {
-    if (!std::is_arithmetic_v<value_type>)
-    {
-        throw type_error("Type must be arithmetic");
-    }
+tensor<_Tp> internal::neon::dot(tensor<_Tp>& t, const tensor<_Tp>& other) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
 
     if (empty() || other.empty())
-    {
         throw std::invalid_argument("Cannot dot product an empty vector");
-    }
 
     if (equal_shape(shape_, shape_type({1})) && equal_shape(other.shape(), shape_type({1})))
-    {
         if (shape_[0] != other.shape()[0])
-        {
-            throw shape_error("Vectors must have the same size for dot product");
-        }
-    }
+            throw error::shape_error("Vectors must have the same size for dot product");
 
     const_pointer     this_data  = data_.data();
     const_pointer     other_data = other.storage().data();
     const std::size_t size       = data_.size();
-    value_type        ret        = 0;
+    _Tp               ret        = 0;
+    const _u64        simd_end   = data_.size() - (data_.size() % t.simd_width);
 
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
-
-    if constexpr (std::is_floating_point_v<value_type>)
+    if constexpr (std::is_floating_point_v<_Tp>)
     {
         std::size_t i       = 0;
         neon_f32    sum_vec = vdupq_n_f32(0.0f);
 
-        for (; i + simd_width <= size; i += simd_width)
+        for (; i + t.simd_width <= size; i += t.simd_width)
         {
             neon_f32 a_vec = vld1q_f32(reinterpret_cast<const _f32*>(&this_data[i]));
             neon_f32 b_vec = vld1q_f32(reinterpret_cast<const _f32*>(&other_data[i]));
@@ -78,16 +63,14 @@ tensor<_Tp> tensor<_Tp>::neon_dot(const tensor& other) const {
         ret                  = vget_lane_f32(vpadd_f32(sum_half, sum_half), 0);
 
         for (; i < size; ++i)
-        {
-            ret += static_cast<value_type>(this_data[i]) * static_cast<value_type>(other_data[i]);
-        }
+            ret += static_cast<_Tp>(this_data[i]) * static_cast<_Tp>(other_data[i]);
     }
-    else if constexpr (std::is_unsigned_v<value_type>)
+    else if constexpr (std::is_unsigned_v<_Tp>)
     {
         std::size_t i       = 0;
         neon_u32    sum_vec = vdupq_n_u32(0.0f);
 
-        for (; i + simd_width <= size; i += simd_width)
+        for (; i + t.simd_width <= size; i += t.simd_width)
         {
             neon_u32 a_vec = vld1q_u32(reinterpret_cast<const _u32*>(&this_data[i]));
             neon_u32 b_vec = vld1q_u32(reinterpret_cast<const _u32*>(&other_data[i]));
@@ -98,16 +81,14 @@ tensor<_Tp> tensor<_Tp>::neon_dot(const tensor& other) const {
         ret                 = vget_lane_u32(vpadd_u32(sum_half, sum_half), 0);
 
         for (; i < size; ++i)
-        {
-            ret += static_cast<value_type>(this_data[i]) * static_cast<value_type>(other_data[i]);
-        }
+            ret += static_cast<_Tp>(this_data[i]) * static_cast<_Tp>(other_data[i]);
     }
-    else if constexpr (std::is_signed_v<value_type>)
+    else if constexpr (std::is_signed_v<_Tp>)
     {
         std::size_t i       = 0;
         neon_s32    sum_vec = vdupq_n_f32(0.0f);
 
-        for (; i + simd_width <= size; i += simd_width)
+        for (; i + t.simd_width <= size; i += t.simd_width)
         {
             neon_s32 a_vec = vld1q_s32(reinterpret_cast<const _s32*>(&this_data[i]));
             neon_s32 b_vec = vld1q_s32(reinterpret_cast<const _s32*>(&other_data[i]));
@@ -118,9 +99,7 @@ tensor<_Tp> tensor<_Tp>::neon_dot(const tensor& other) const {
         ret                = vget_lane_s32(vpadd_s32(sum_half, sum_half), 0);
 
         for (; i < size; ++i)
-        {
-            ret += static_cast<value_type>(this_data[i]) * static_cast<value_type>(other_data[i]);
-        }
+            ret += static_cast<_Tp>(this_data[i]) * static_cast<_Tp>(other_data[i]);
     }
 
     return self({ret}, {1});
