253e2e81582f1c6a3eb9d5fb6bf93f4df98025b2
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-21 13:25:43 +0100

    stuff

--- src/simd/linear/sigmoid.hpp
+++ src/simd/linear/sigmoid.hpp
@@ -3,33 +3,28 @@
 #include "tensorbase.hpp"
 
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_sigmoid_() {
-    if (!std::is_arithmetic_v<value_type>)
+tensor<_Tp>& internal::neon::sigmoid_(tensor<_Tp>& t) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
+
+    std::vector<_Tp>& data_ = t.storage_();
+    using neon_type         = typename std::conditional<std::is_same_v<_Tp, _f32>, neon_f32, void>::type;
+    const _u64 simd_end     = data_.size() - (data_.size() % t.simd_width);
+    _u64       i            = 0;
+
+    if constexpr (std::is_same_v<_Tp, _f32>)
     {
-        throw type_error("Type must be arithmetic");
-    }
-
-    index_type i = 0;
-
-    using neon_type           = typename std::conditional<std::is_same_v<value_type, _f32>, neon_f32, void>::type;
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
-
-    if constexpr (std::is_same_v<value_type, _f32>)
-    {
-        for (; i < simd_end; i += simd_width)
+        for (; i < simd_end; i += t.simd_width)
         {
             neon_type v         = vld1q_f32(reinterpret_cast<const _f32*>(&data_[i]));
             neon_type exp_neg_v = vexpq_f32(vnegq_f32(v));                               // e^(-x)
             neon_type sigmoid   = vrecpeq_f32(vaddq_f32(vdupq_n_f32(1.0f), exp_neg_v));  // 1 / (1 + e^(-x))
-
             vst1q_f32(reinterpret_cast<_f32*>(&data_[i]), sigmoid);
         }
     }
 
     for (; i < data_.size(); ++i)
-    {
-        data_[i] = static_cast<value_type>(1.0 / (1.0 + std::exp(-static_cast<double>(data_[i]))));
-    }
+        data_[i] = static_cast<_Tp>(1.0 / (1.0 + std::exp(-static_cast<double>(data_[i]))));
 
     return *this;
 }
