4c50aa61aa2c0454c73d72b10be9d4d88862b5dc
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-27 17:30:36 +0100

    stuff

--- src/simd/arithmetic/sqrt.hpp
+++ src/simd/arithmetic/sqrt.hpp
@@ -2,40 +2,36 @@
 
 #include "tensorbase.hpp"
 
+
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_sqrt_() {
-    if (!std::is_arithmetic_v<value_type>)
+tensor<_Tp>& internal::neon::sqrt_(tensor<_Tp>& t) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
+
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
+    _u64              i        = 0;
+
+    for (; i < simd_end; i += t.simd_width)
     {
-        throw type_error("Type must be arithmetic");
-    }
+        neon_type<_Tp>  data_vec = neon_load<_Tp>(&data_[i]);
+        alignas(16) _Tp vals[t.simd_width];
+        neon_load<_Tp>(vals, data_vec);
 
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
-
-    index_type i = 0;
-    for (; i < simd_end; i += simd_width)
-    {
-        neon_type<value_type>  data_vec = neon_load<value_type>(&data_[i]);
-        alignas(16) value_type vals[simd_width];
-        neon_load<value_type>(vals, data_vec);
-
-        for (int j = 0; j < simd_width; ++j)
+        for (int j = 0; j < t.simd_width; ++j)
         {
-            if (vals[j] < static_cast<value_type>(0))
-            {
+            if (vals[j] < static_cast<_Tp>(0))
                 throw std::domain_error("Cannot get the square root of a negative number");
-            }
 
-            vals[j] = static_cast<value_type>(std::sqrt(vals[j]));
+            vals[j] = static_cast<_Tp>(std::sqrt(vals[j]));
         }
 
-        neon_type<value_type> sqrt_vec = neon_load<value_type>(vals);
-        neon_store(&data_[i], sqrt_vec);
+        neon_type<_Tp> sqrt_vec = neon_load<_Tp>(vals);
+        neon_store<_Tp>(&data_[i], sqrt_vec);
     }
 
     for (; i < data_.size(); ++i)
-    {
-        data_[i] = static_cast<value_type>(std::sqrt(data_[i]));
-    }
+        data_[i] = static_cast<_Tp>(std::sqrt(data_[i]));
 
-    return *this;
+    return t;
 }
