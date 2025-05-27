4087cf261db791f5f9b41d2ba53363e8fe07c19a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-05-27 17:31:33 +0100

    stuff

--- src/simd/arithmetic/pow.hpp
+++ src/simd/arithmetic/pow.hpp
@@ -2,63 +2,62 @@
 
 #include "tensorbase.hpp"
 
+
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_pow_(const value_type value) {
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
+tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const _Tp value) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
 
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
+    _u64              i        = 0;
 
-    index_type i = 0;
-    for (; i < simd_end; i += simd_width)
+    for (; i < simd_end; i += t.simd_width)
     {
-        neon_type<value_type>  data_vec = neon_load<value_type>(&data_[i]);
-        alignas(16) value_type vals[simd_width];
-        neon_load<value_type>(vals, data_vec);
+        neon_type<_Tp>  data_vec = neon_load<_Tp>(&data_[i]);
+        alignas(16) _Tp vals[t.simd_width];
+        neon_load<_Tp>(vals, data_vec);
 
-        for (int j = 0; j < simd_width; ++j)
-            vals[j] = static_cast<value_type>(std::pow(vals[j], value));
+        for (int j = 0; j < t.simd_width; ++j)
+            vals[j] = static_cast<_Tp>(std::pow(vals[j], value));
 
-        neon_type<value_type> pow_vec = neon_load<value_type>(vals);
-        neon_store<value_type>(&data_[i], pow_vec);
+        neon_type<_Tp> pow_vec = neon_load<_Tp>(vals);
+        neon_store<_Tp>(&data_[i], pow_vec);
     }
 
     for (; i < data_.size(); ++i)
-        data_[i] = static_cast<value_type>(std::pow(data_[i], value));
+        data_[i] = static_cast<_Tp>(std::pow(data_[i], value));
 
-    return *this;
+    return t;
 }
 
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_pow_(const tensor& other) {
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
+tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const tensor<_Tp>& other) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
 
-    if (!equal_shape(shape(), other.shape()))
-        throw shape_error("Tensors shapes must be equal");
+    if (!t.shape().equal(other.shape()))
+        throw error::shape_error("Tensors shapes must be equal");
 
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
+    _u64              i        = 0;
 
-    index_type i = 0;
-    for (; i < simd_end; i += simd_width)
+    for (; i < simd_end; i += t.simd_width)
     {
-        neon_type<value_type> base_vec   = neon_load<value_type>(&data_[i]);
-        neon_type<value_type> exp_vec    = neon_load<value_type>(&other[i]);
-        neon_type<value_type> result_vec = {
-          static_cast<value_type>(
-            std::pow(neon_get_lane<value_type>(base_vec, 0), neon_get_lane<value_type>(exp_vec, 0))),
-          static_cast<value_type>(
-            std::pow(neon_get_lane<value_type>(base_vec, 1), neon_get_lane<value_type>(exp_vec, 1))),
-          static_cast<value_type>(
-            std::pow(neon_get_lane<value_type>(base_vec, 2), neon_get_lane<value_type>(exp_vec, 2))),
-          static_cast<value_type>(
-            std::pow(neon_get_lane<value_type>(base_vec, 3), neon_get_lane<value_type>(exp_vec, 3))),
+        neon_type<_Tp> base_vec   = neon_load<_Tp>(&data_[i]);
+        neon_type<_Tp> exp_vec    = neon_load<_Tp>(&other[i]);
+        neon_type<_Tp> result_vec = {
+          static_cast<_Tp>(std::pow(neon_get_lane<_Tp>(base_vec, 0), neon_get_lane<_Tp>(exp_vec, 0))),
+          static_cast<_Tp>(std::pow(neon_get_lane<_Tp>(base_vec, 1), neon_get_lane<_Tp>(exp_vec, 1))),
+          static_cast<_Tp>(std::pow(neon_get_lane<_Tp>(base_vec, 2), neon_get_lane<_Tp>(exp_vec, 2))),
+          static_cast<_Tp>(std::pow(neon_get_lane<_Tp>(base_vec, 3), neon_get_lane<_Tp>(exp_vec, 3))),
         };
-        neon_store<value_type>(&data_[i], result_vec);
+        neon_store<_Tp>(&data_[i], result_vec);
     }
 
     for (; i < data_.size(); ++i)
-        data_[i] = static_cast<value_type>(std::pow((_f32) data_[i], (_f32) other[i]));
+        data_[i] = static_cast<_Tp>(std::pow((_f32) data_[i], (_f32) other[i]));
 
-    return *this;
+    return t;
 }
