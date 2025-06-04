d6dd873cfb0115e3e4b49eef68b6e4fed53cc7e6
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-04 18:00:34 +0100

    stuff

--- src/logical/xor.hpp
+++ src/logical/xor.hpp
@@ -3,25 +3,15 @@
 #include "tensorbase.hpp"
 
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::logical_xor_(const value_type value) {
-    if constexpr (!std::is_integral_v<value_type>)
+inline tensor<_Tp>& tensor<_Tp>::logical_xor_(const value_type value) {
+    if (internal::types::using_neon())
     {
-        throw type_error("Cannot get the element wise xor of non-integral and non-boolean value");
+        return internal::neon::logical_xor_(value);
     }
 
-    for (auto& elem : data_)
-    {
-        elem = (elem xor value);
-    }
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::logical_xor_(const value_type value) const {
     if constexpr (!std::is_integral_v<value_type>)
     {
-        throw type_error("Cannot get the element wise xor of non-integral and non-boolean value");
+        throw error::type_error("Cannot get the element wise xor of non-integral and non-boolean value");
     }
 
     for (auto& elem : data_)
@@ -48,38 +38,23 @@ tensor<_Tp> tensor<_Tp>::logical_xor(const value_type value) const {
 
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::logical_xor_(const tensor& other) {
+    if (internal::types::using_neon())
+    {
+        return internal::neon::logical_xor_(other);
+    }
+
     if constexpr (!std::is_integral_v<value_type>)
     {
-        throw type_error("Cannot get the element wise xor of non-integral and non-boolean value");
+        throw error::type_error("Cannot get the element wise xor of non-integral and non-boolean value");
     }
 
     if (!equal_shape(shape(), other.shape()))
     {
-        throw shape_error("Tensors shapes must be equal");
+        throw error::shape_error("Tensors shapes must be equal");
     }
 
     index_type i = 0;
-    for (auto& elem : data_)
-    {
-        elem = (elem xor other[i++]);
-    }
 
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::logical_xor_(const tensor& other) const {
-    if constexpr (!std::is_integral_v<value_type>)
-    {
-        throw type_error("Cannot get the element wise xor of non-integral and non-boolean value");
-    }
-
-    if (!equal_shape(shape(), other.shape()))
-    {
-        throw shape_error("Tensors shapes must be equal");
-    }
-
-    index_type i = 0;
     for (auto& elem : data_)
     {
         elem = (elem xor other[i++]);
