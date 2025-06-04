706281d0a51a75a7c04b2e1b5aff2d57fd3c6226
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-04 18:00:04 +0100

    stuff

--- src/simd/arithmetic/abs.hpp
+++ src/simd/arithmetic/abs.hpp
@@ -4,31 +4,26 @@
 #include "tensorbase.hpp"
 
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::neon_abs_() {
-    if (!std::is_arithmetic_v<value_type>)
-    {
-        throw type_error("Type must be arithmetic");
-    }
+tensor<_Tp>& internal::neon::abs_(tensor<_Tp>& t) {
+    if (!std::is_arithmetic_v<_Tp>)
+        throw error::type_error("Type must be arithmetic");
 
-    if (std::is_unsigned_v<value_type>)
-    {
-        return *this;
-    }
+    if (std::is_unsigned_v<_Tp>)
+        return t;
 
-    const index_type simd_end = data_.size() - (data_.size() % simd_width);
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
+    _u64              i        = 0;
 
-    index_type i = 0;
-    for (; i < simd_end; i += simd_width)
+    for (; i < simd_end; i += t.simd_width)
     {
-        neon_type<value_type> vec     = neon_load<value_type>(&data_[i]);
-        neon_type<value_type> abs_vec = neon_abs<value_type>(vec);
-        neon_store<value_type>(&data_[i], abs_vec);
+        neon_type<_Tp> vec     = neon_load<_Tp>(&data_[i]);
+        neon_type<_Tp> abs_vec = neon_abs<_Tp>(vec);
+        neon_store<_Tp>(&data_[i], abs_vec);
     }
 
     for (; i < data_.size(); ++i)
-    {
-        data_[i] = static_cast<value_type>(std::abs(data_[i]));
-    }
+        data_[i] = static_cast<_Tp>(std::abs(data_[i]));
 
-    return *this;
+    return t;
 }
