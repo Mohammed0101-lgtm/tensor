e37f8d0fb74951e95f5927de60d14045d307708f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-06 15:07:42 +0100

    stuff

--- src/logical/and.hpp
+++ src/logical/and.hpp
@@ -3,26 +3,22 @@
 #include "tensorbase.hpp"
 
 
-namespace logical {
-
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::logical_and_(const value_type value) {
+    if (internal::types::using_neon())
+    {
+        return internal::neon::logical_and_(value);
+    }
+
     if (!std::is_integral_v<value_type>)
-        throw type_error("Cannot get the element wise and of non-integral and non-boolean value");
+    {
+        throw error::type_error("Cannot get the element wise and of non-integral and non-boolean value");
+    }
 
     for (auto& elem : data_)
+    {
         elem = (elem && value);
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::logical_and_(const value_type value) const {
-    if (!std::is_integral_v<value_type>)
-        throw type_error("Cannot get the element wise and of non-integral and non-boolean value");
-
-    for (auto& elem : data_)
-        elem = (elem && value);
+    }
 
     return *this;
 }
@@ -43,32 +39,28 @@ tensor<_Tp> tensor<_Tp>::logical_and(const value_type value) const {
 
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::logical_and_(const tensor& other) {
+    if (internal::types::using_neon())
+    {
+        return internal::neon::logical_and_(other);
+    }
+
     if (!std::is_integral_v<value_type>)
-        throw type_error("Cannot get the element-wise and of non-integral and non-boolean value");
+    {
+        throw error::type_error("Cannot get the element-wise and of non-integral and non-boolean value");
+    }
 
     if (!equal_shape(shape(), other.shape()))
-        throw shape_error("Tensors shapes must be equal");
+    {
+        throw error::shape_error("Tensors shapes must be equal");
+    }
 
     index_type i = 0;
+
     for (auto& elem : data_)
+
+    {
         elem = (elem && other[i++]);
+    }
 
     return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::logical_and_(const tensor& other) const {
-    if (!std::is_integral_v<value_type>)
-        throw type_error("Cannot get the element-wise and of non-integral and non-boolean value");
-
-    if (!equal_shape(shape(), other.shape()))
-        throw shape_error("Tensors shapes must be equal");
-
-    index_type i = 0;
-    for (auto& elem : data_)
-        elem = (elem && other[i++]);
-
-    return *this;
-}
-
 }
