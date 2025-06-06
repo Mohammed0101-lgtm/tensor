d4d0f4da8a21fcd1d5efc1a7ea7c64651867ce45
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-06 15:07:34 +0100

    stuff

--- src/logical/not.hpp
+++ src/logical/not.hpp
@@ -10,13 +10,6 @@ inline tensor<_Tp>& tensor<_Tp>::logical_not_() {
 }
 
 template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::logical_not_() const {
-    bitwise_not_();
-    bool_();
-    return *this;
-}
-
-template<class _Tp>
 tensor<bool> tensor<_Tp>::logical_not() const {
     tensor<bool> ret = bool_();
     ret.logical_not_();
