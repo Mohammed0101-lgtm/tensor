88a60b6fd70506b827e6fe94accd26736fa7822e
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-06 15:09:07 +0100

    stuff

--- src/linear/sigmoid.hpp
+++ src/linear/sigmoid.hpp
@@ -13,22 +13,7 @@ template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::sigmoid_() {
     if (!std::is_arithmetic_v<value_type>)
     {
-        throw type_error("Type must be arithmetic");
-    }
-
-    for (auto& elem : data_)
-    {
-        elem = 1.0 / (1.0 + std::exp(-elem));
-    }
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::sigmoid_() const {
-    if (!std::is_arithmetic_v<value_type>)
-    {
-        throw type_error("Type must be arithmetic");
+        throw error::type_error("Type must be arithmetic");
     }
 
     for (auto& elem : data_)
