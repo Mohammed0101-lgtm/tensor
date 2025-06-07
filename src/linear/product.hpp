f0b6739fb0deb474e0e501966dd4e22818f3f9c4
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-07 14:01:56 +0100

    stuff

--- src/linear/product.hpp
+++ src/linear/product.hpp
@@ -5,10 +5,14 @@
 template<class _Tp>
 tensor<_Tp> tensor<_Tp>::cross_product(const tensor& other) const {
     if (empty() || other.empty())
+    {
         throw std::invalid_argument("Cannot cross product an empty vector");
+    }
 
     if (shape() != shape_type{3} || other.shape() != shape_type{3})
+    {
         throw std::logic_error("Cross product can only be performed on 3-element vectors");
+    }
 
     tensor ret({3});
 
@@ -64,12 +68,16 @@ global void cross_product_kernel(_Tp* a, _Tp* b, _Tp* c) {
 template<class _Tp>
 tensor<_Tp> tensor<_Tp>::dot(const tensor& other) const {
     if (empty() || other.empty())
+    {
         throw std::invalid_argument("Cannot dot product an empty vector");
+    }
 
     if (shape_.size() == 1 && other.shape().size() == 1)
     {
         if (shape_[0] != other.shape()[0])
-            throw shape_error("Vectors must have the same size for dot product");
+        {
+            throw error::shape_error("Vectors must have the same size for dot product");
+        }
 
         const_pointer     this_data     = data_.data();
         data_t            other_storage = other.storage();
@@ -81,10 +89,14 @@ tensor<_Tp> tensor<_Tp>::dot(const tensor& other) const {
     }
 
     if (shape_.size() == 2 && other.shape().size() == 2)
+    {
         return matmul(other);
+    }
 
     if (shape_.size() == 3 && other.shape().size() == 3)
+    {
         return cross_product(other);
+    }
 
     return self();
 }
@@ -95,27 +107,29 @@ tensor<_Tp> tensor<_Tp>::cumprod(index_type dimension) const {
     {
         data_t flat = data_;
         data_t ret(flat.size());
-        ret[0] = flat[0];
-
+        ret[0]       = flat[0];
         index_type i = 1;
 
         for (; i < flat.size(); ++i)
+        {
             ret[i] = ret[i - 1] * flat[i];
+        }
 
         return self(ret, {flat.size()});
     }
     else
     {
         if (dimension < 0 || dimension >= static_cast<index_type>(shape_.size()))
-            throw index_error("Invalid dimension provided for cumprod.");
+        {
+            throw error::index_error("Invalid dimension provided for cumprod.");
+        }
 
         data_t ret(data_);
         // TODO : compute_outer_size() implementation
         index_type outer_size = compute_outer_size(dimension);
         index_type inner_size = shape_[dimension];
-        index_type st         = strides_[dimension];
-
-        index_type i = 0;
+        index_type st         = shape_.strides_[dimension];
+        index_type i          = 0;
 
         for (; i < outer_size; ++i)
         {
