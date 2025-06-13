fdb5abea4a148321da2734684515feda7786425a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-13 19:28:35 +0100

    stuff

--- src/simd/linear.hpp
+++ src/simd/linear.hpp
@@ -3,8 +3,59 @@
 #include "tensorbase.hpp"
 #include "types.hpp"
 
+
+
 template<class _Tp>
-tensor<_Tp> internal::neon::absolute_(tensor<_Tp>& t, const tensor<_Tp>& other) {
+tensor<_Tp> internal::neon::absolute(const tensor<_Tp>& t, const tensor<_Tp>& other) {
+    if (!std::is_arithmetic_v<_Tp>)
+    {
+        throw error::type_error("Type must be arithmetic");
+    }
+
+    if (std::is_unsigned_v<_Tp>)
+    {
+        return t;
+    }
+
+    if (!t.shape().equal(other.shape()))
+    {
+        throw error::shape_error("Tensors must have the same shape");
+    }
+
+    std::vector<_Tp>& data_ = t.storage_();
+    _u64              s     = t.storage().size();
+    std::vector<_Tp>  a(s);
+    _u64              i = 0;
+
+    if constexpr (std::is_floating_point_v<_Tp>)
+    {
+        for (; i < data_.size(); i += _ARM64_REG_WIDTH)
+        {
+            neon_f32 data_vec = vld1q_f32(reinterpret_cast<const _f32*>(&data_[i]));
+            neon_f32 abs_vec  = vabsq_f32(data_vec);
+            vst1q_f32(reinterpret_cast<_f32*>(&a[i]), abs_vec);
+        }
+    }
+    else if constexpr (std::is_signed_v<_Tp>)
+    {
+        for (; i < data_.size(); i += _ARM64_REG_WIDTH)
+        {
+            neon_s32 data_vec = vld1q_s32(reinterpret_cast<const _s32*>(&data_[i]));
+            neon_s32 abs_vec  = vabsq_s32(data_vec);
+            vst1q_s32(reinterpret_cast<_s32*>(&a[i]), abs_vec);
+        }
+    }
+
+    for (; i < s; ++i)
+    {
+        a[i] = static_cast<_Tp>(std::abs(data_[i]));
+    }
+
+    return tensor<_Tp>(t.shape(), a);
+}
+
+template<class _Tp>
+tensor<_Tp> internal::neon::absolute_(const tensor<_Tp>& t, const tensor<_Tp>& other) {
     if (!std::is_arithmetic_v<_Tp>)
     {
         throw error::type_error("Type must be arithmetic");
