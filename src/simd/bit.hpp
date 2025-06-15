8366b58dd106f363a9e02dc979ba1a169b263e98
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-15 12:19:45 +0100

    stuff

--- src/simd/bit.hpp
+++ src/simd/bit.hpp
@@ -9,21 +9,20 @@ tensor<_Tp>& internal::neon::bitwise_right_shift_(tensor<_Tp>& t, const int amou
         throw error::type_error("Type must be integral");
     }
 
-    std::vector<_Tp>&    data_            = t.storage_();
-    const _u64           simd_end         = data_.size() - (data_.size() % t.simd_width);
-    const neon_type<_Tp> shift_amount_vec = neon_dup<_Tp>(-amount);
-    _u64                 i                = 0;
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
+    _u64              i        = 0;
 
     for (; i < simd_end; i += t.simd_width)
     {
         neon_type<_Tp> data_vec    = neon_load<_Tp>(&data_[i]);
-        neon_type<_Tp> shifted_vec = neon_shl<_Tp>(data_vec, shift_amount_vec);
+        neon_type<_Tp> shifted_vec = neon_shr<_Tp>(data_vec, amount);  // Use correct NEON right shift
         neon_store<_Tp>(&data_[i], shifted_vec);
     }
 
     for (; i < data_.size(); ++i)
     {
-        data_[i] >>= amount;
+        data_[i] = data_[i] >> amount;
     }
 
     return t;
@@ -31,7 +30,28 @@ tensor<_Tp>& internal::neon::bitwise_right_shift_(tensor<_Tp>& t, const int amou
 
 template<class _Tp>
 tensor<_Tp>& internal::neon::bitwise_left_shift_(tensor<_Tp>& t, const int amount) {
-    return neon_bitwise_right_shift_(t, -amount);
+    if (!std::is_integral_v<_Tp>)
+    {
+        throw error::type_error("Type must be integral");
+    }
+
+    std::vector<_Tp>& data_    = t.storage_();
+    const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
+    _u64              i        = 0;
+
+    for (; i < simd_end; i += t.simd_width)
+    {
+        neon_type<_Tp> data_vec    = neon_load<_Tp>(&data_[i]);
+        neon_type<_Tp> shifted_vec = neon_shl<_Tp>(data_vec, amount);
+        neon_store<_Tp>(&data_[i], shifted_vec);
+    }
+
+    for (; i < data_.size(); ++i)
+    {
+        data_[i] = data_[i] << amount;
+    }
+
+    return t;
 }
 
 template<class _Tp>
@@ -203,10 +223,10 @@ tensor<_Tp>& internal::neon::bitwise_or_(tensor<_Tp>& t, const tensor<_Tp>& othe
 
     for (; i < simd_end; i += t.simd_width)
     {
-        neon_type<_Tp> data_vec  = neon_load<_Tp>(&data_vec[i]);
+        neon_type<_Tp> data_vec  = neon_load<_Tp>(&data_[i]);
         neon_type<_Tp> other_vec = neon_load<_Tp>(&other[i]);
         neon_type<_Tp> xor_vec   = neon_or<_Tp>(data_vec, other_vec);
-        neon_store<_Tp>(&data_vec[i], xor_vec);
+        neon_store<_Tp>(&data_[i], xor_vec);
     }
 
     for (; i < data_.size(); ++i)
