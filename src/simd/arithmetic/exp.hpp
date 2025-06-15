1b37771160ffe5fe83f1d7195fccd973e2dbdcd5
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-15 12:21:24 +0100

    stuff

--- src/simd/arithmetic/exp.hpp
+++ src/simd/arithmetic/exp.hpp
@@ -5,7 +5,9 @@
 template<class _Tp>
 tensor<_Tp>& internal::neon::exp_(tensor<_Tp>& t) {
     if (!std::is_arithmetic_v<_Tp>)
+    {
         throw error::type_error("Type must be arithmetic");
+    }
 
     std::vector<_Tp>& data_    = t.storage_();
     const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
@@ -18,14 +20,18 @@ tensor<_Tp>& internal::neon::exp_(tensor<_Tp>& t) {
         neon_store<_Tp>(vals, vec);
 
         for (int j = 0; j < t.simd_width; ++j)
+        {
             vals[j] = static_cast<_Tp>(std::exp(vals[0]));
+        }
 
         neon_type<_Tp> exp_vec = neon_load<_Tp>(vals);
         neon_store<_Tp>(&data_[i], exp_vec);
     }
 
     for (; i < data_.size(); ++i)
+    {
         data_[i] = static_cast<_Tp>(std::exp(data_[i]));
+    }
 
     return t;
 }
