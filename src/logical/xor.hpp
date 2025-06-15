88899e6b537e449360604aec6e7f8b86b7a0f8db
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-15 12:22:50 +0100

    stuff

--- src/logical/xor.hpp
+++ src/logical/xor.hpp
@@ -2,6 +2,7 @@
 
 #include "tensorbase.hpp"
 
+
 template<class _Tp>
 inline tensor<_Tp>& tensor<_Tp>::logical_xor_(const value_type value) {
     if (internal::types::using_neon())
@@ -24,6 +25,11 @@ inline tensor<_Tp>& tensor<_Tp>::logical_xor_(const value_type value) {
 
 template<class _Tp>
 tensor<_Tp> tensor<_Tp>::logical_xor(const tensor& other) const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.logical_xor_(other);
     return ret;
@@ -31,6 +37,11 @@ tensor<_Tp> tensor<_Tp>::logical_xor(const tensor& other) const {
 
 template<class _Tp>
 tensor<_Tp> tensor<_Tp>::logical_xor(const value_type value) const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.logical_xor(value);
     return ret;
@@ -48,7 +59,7 @@ tensor<_Tp>& tensor<_Tp>::logical_xor_(const tensor& other) {
         throw error::type_error("Cannot get the element wise xor of non-integral and non-boolean value");
     }
 
-    if (!equal_shape(shape(), other.shape()))
+    if (!shape().equal(other.shape()))
     {
         throw error::shape_error("Tensors shapes must be equal");
     }
