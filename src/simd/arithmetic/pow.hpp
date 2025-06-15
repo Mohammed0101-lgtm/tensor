8b2d06b850c2a3189c5b9fb53e95955256f84b9d
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-15 12:20:30 +0100

    stuff

--- src/simd/arithmetic/pow.hpp
+++ src/simd/arithmetic/pow.hpp
@@ -6,7 +6,9 @@
 template<class _Tp>
 tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const _Tp value) {
     if (!std::is_arithmetic_v<_Tp>)
+    {
         throw error::type_error("Type must be arithmetic");
+    }
 
     std::vector<_Tp>& data_    = t.storage_();
     const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
@@ -16,17 +18,21 @@ tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const _Tp value) {
     {
         neon_type<_Tp>  data_vec = neon_load<_Tp>(&data_[i]);
         alignas(16) _Tp vals[t.simd_width];
-        neon_load<_Tp>(vals, data_vec);
+        neon_store<_Tp>(vals, data_vec);
 
         for (int j = 0; j < t.simd_width; ++j)
+        {
             vals[j] = static_cast<_Tp>(std::pow(vals[j], value));
+        }
 
         neon_type<_Tp> pow_vec = neon_load<_Tp>(vals);
         neon_store<_Tp>(&data_[i], pow_vec);
     }
 
     for (; i < data_.size(); ++i)
+    {
         data_[i] = static_cast<_Tp>(std::pow(data_[i], value));
+    }
 
     return t;
 }
@@ -34,10 +40,14 @@ tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const _Tp value) {
 template<class _Tp>
 tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const tensor<_Tp>& other) {
     if (!std::is_arithmetic_v<_Tp>)
+    {
         throw error::type_error("Type must be arithmetic");
+    }
 
     if (!t.shape().equal(other.shape()))
+    {
         throw error::shape_error("Tensors shapes must be equal");
+    }
 
     std::vector<_Tp>& data_    = t.storage_();
     const _u64        simd_end = data_.size() - (data_.size() % t.simd_width);
@@ -57,7 +67,9 @@ tensor<_Tp>& internal::neon::pow_(tensor<_Tp>& t, const tensor<_Tp>& other) {
     }
 
     for (; i < data_.size(); ++i)
+    {
         data_[i] = static_cast<_Tp>(std::pow((_f32) data_[i], (_f32) other[i]));
+    }
 
     return t;
 }
