9576a7d186afa6d1c7f62acecd56c602b0447f9b
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-19 18:54:13 +0100

    stuff

--- src/arithmetic/frac.hpp
+++ src/arithmetic/frac.hpp
@@ -2,26 +2,22 @@
 
 #include "tensorbase.hpp"
 
-template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::frac_() {
-    if (!std::is_floating_point_v<value_type>)
-    {
-        throw type_error("Type must be floating point");
-    }
-
-    for (auto& elem : data_)
-    {
-        elem = frac(elem);
-    }
-
-    return *this;
-}
 
 template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::frac_() const {
+inline tensor<_Tp>& tensor<_Tp>::frac_() {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::frac_(*this);
+    }
+
     if (!std::is_floating_point_v<value_type>)
     {
-        throw type_error("Type must be floating point");
+        throw error::type_error("Type must be floating point");
     }
 
     for (auto& elem : data_)
@@ -34,6 +30,11 @@ inline const tensor<_Tp>& tensor<_Tp>::frac_() const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::frac() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.frac_();
     return ret;
