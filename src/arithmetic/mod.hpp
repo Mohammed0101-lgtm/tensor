4ff87c574aaf53d028ff8c36f8502f8ae2353bf8
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-19 18:53:12 +0100

    stuff

--- src/arithmetic/mod.hpp
+++ src/arithmetic/mod.hpp
@@ -4,6 +4,11 @@
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::fmod(const tensor& other) const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.fmod_(other);
     return ret;
@@ -11,6 +16,11 @@ inline tensor<_Tp> tensor<_Tp>::fmod(const tensor& other) const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::fmod(const value_type value) const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.fmod_(value);
     return ret;
@@ -18,29 +28,19 @@ inline tensor<_Tp> tensor<_Tp>::fmod(const value_type value) const {
 
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::fmod_(const value_type value) {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::fmod_(*this, value);
+    }
+
     if (!std::is_floating_point_v<value_type>)
     {
-        throw type_error("Cannot perform fmod on non-floating point values");
-    }
-
-    if (!value)
-    {
-        throw std::logic_error("Cannot perform fmod with zero");
-    }
-
-    for (auto& elem : data_)
-    {
-        elem = std::fmod(elem, value);
-    }
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::fmod_(const value_type value) const {
-    if (!std::is_floating_point_v<value_type>)
-    {
-        throw type_error("Cannot perform fmod on non-floating point values");
+        throw error::type_error("Cannot perform fmod on non-floating point values");
     }
 
     if (!value)
@@ -58,14 +58,24 @@ inline const tensor<_Tp>& tensor<_Tp>::fmod_(const value_type value) const {
 
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::fmod_(const tensor& other) {
-    if (!equal_shape(shape(), other.shape()) || data_.size() != other.size(0))
+    if (empty())
     {
-        throw shape_error("Cannot divide two tensors of different shapes : fmod");
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::fmod_(*this, other);
+    }
+
+    if (!shape().equal(other.shape()) || data_.size() != other.size(0))
+    {
+        throw error::shape_error("Cannot divide two tensors of different shapes : fmod");
     }
 
     if (!std::is_floating_point_v<value_type>)
     {
-        throw type_error("Cannot perform fmod on non-floating point values");
+        throw error::type_error("Cannot perform fmod on non-floating point values");
     }
 
     if (other.count_nonzero(0) != other.size(0))
@@ -74,32 +84,7 @@ tensor<_Tp>& tensor<_Tp>::fmod_(const tensor& other) {
     }
 
     index_type i = 0;
-    for (auto& elem : data_)
-    {
-        elem = std::fmod(elem, other[i++]);
-    }
 
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::fmod_(const tensor& other) const {
-    if (equal_shape(shape(), other.shape()) || data_.size() != other.size(0))
-    {
-        throw shape_error("Cannot divide two tensors of different shapes : fmod");
-    }
-
-    if (!std::is_floating_point_v<value_type>)
-    {
-        throw type_error("Cannot perform fmod on non-floating point values");
-    }
-
-    if (other.count_nonzero(0) != other.size(0))
-    {
-        throw std::logic_error("Cannot divide by zero : undefined operation");
-    }
-
-    index_type i = 0;
     for (auto& elem : data_)
     {
         elem = std::fmod(elem, other[i++]);
