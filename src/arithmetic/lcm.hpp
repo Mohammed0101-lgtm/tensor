e090cfee404ce35458f49bf6687947231b2f04dd
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-19 18:53:58 +0100

    stuff

--- src/arithmetic/lcm.hpp
+++ src/arithmetic/lcm.hpp
@@ -2,6 +2,7 @@
 
 #include "tensorbase.hpp"
 
+
 // used as a helper function
 inline int64_t __lcm(const int64_t a, const int64_t b) { return (a * b) / std::gcd(a, b); }
 
@@ -9,7 +10,7 @@ template<class _Tp>
 inline typename tensor<_Tp>::index_type tensor<_Tp>::lcm() const {
     if (!std::is_arithmetic_v<value_type>)
     {
-        throw type_error("Type must be arithmetic");
+        throw error::type_error("Type must be arithmetic");
     }
 
     index_type ret = static_cast<index_type>(data_[0]);
@@ -24,17 +25,22 @@ inline typename tensor<_Tp>::index_type tensor<_Tp>::lcm() const {
 
 template<class _Tp>
 tensor<_Tp> tensor<_Tp>::lcm(const tensor& other) const {
-    if (!equal_shape(shape(), other.shape()))
+    if (empty())
     {
-        throw shape_error("Tensors shapes must be equal");
+        return self({0});
     }
 
-    tensor ret = clone();
+    if (!shape().equal(other.shape()))
+    {
+        throw error::shape_error("Tensors shapes must be equal");
+    }
 
-    index_type i = 0;
+    tensor     ret = clone();
+    index_type i   = 0;
+
     for (const auto& elem : data_)
     {
-        ret[i++] = lcm(elem, other[i]);
+        ret[i++] = __lcm(elem, other[i]);
     }
 
     return ret;
