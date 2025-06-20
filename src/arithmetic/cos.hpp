53fd0f79cfd4d924ca0dd840339b61fda4cedf4a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-20 12:30:50 +0100

    stuff_2

--- src/arithmetic/cos.hpp
+++ src/arithmetic/cos.hpp
@@ -3,52 +3,53 @@
 #include "tensorbase.hpp"
 
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::cos_() {
+inline tensor<_Tp>& tensor<_Tp>::cos_() {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::cos_(*this);
+    }
+
     if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
-
-    for (auto& elem : data_)
-        elem = std::cos(elem);
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::cos_() const {
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
-
-    for (auto& elem : data_)
-        elem = std::cos(elem);
-
-    return *this;
-}
-
-template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::acos_() {
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
+    {
+        throw error::type_error("Type must be arithmetic");
+    }
 
     for (auto& elem : data_)
     {
-        if (elem > 1.0 || elem < -1.0)
-            throw std::domain_error("Input data is out of domain for acos()");
-
-        elem = std::acos(elem);
+        elem = std::cos(elem);
     }
 
     return *this;
 }
 
 template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::acos_() const {
+tensor<_Tp>& tensor<_Tp>::acos_() {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::acos_(*this);
+    }
+
     if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
+    {
+        throw error::type_error("Type must be arithmetic");
+    }
 
     for (auto& elem : data_)
     {
-        if (elem > 1.0 || elem < -1.0)
+        if (elem > _Tp(1.0) || elem < _Tp(-1.0))
+        {
             throw std::domain_error("Input data is out of domain for acos()");
+        }
 
         elem = std::acos(elem);
     }
@@ -58,6 +59,11 @@ inline const tensor<_Tp>& tensor<_Tp>::acos_() const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::acos() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.acos_();
     return ret;
@@ -65,6 +71,11 @@ inline tensor<_Tp> tensor<_Tp>::acos() const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::cos() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.cos_();
     return ret;
@@ -72,58 +83,64 @@ inline tensor<_Tp> tensor<_Tp>::cos() const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::cosh() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.cosh_();
     return ret;
 }
 
 template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::cosh_() {
+inline tensor<_Tp>& tensor<_Tp>::cosh_() {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::cosh_(*this);
+    }
+
     if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
-
-    for (auto& elem : data_)
-        elem = std::cosh(elem);
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::cosh_() const {
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
-
-    for (auto& elem : data_)
-        elem = std::cosh(elem);
-
-    return *this;
-}
-
-template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::acosh_() {
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
+    {
+        throw error::type_error("Type must be arithmetic");
+    }
 
     for (auto& elem : data_)
     {
-        if (elem < 1.0)
-            throw std::domain_error("Input data is out of domain of acosh()");
-
-        elem = std::acosh(elem);
+        elem = std::cosh(elem);
     }
 
     return *this;
 }
 
 template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::acosh_() const {
+tensor<_Tp>& tensor<_Tp>::acosh_() {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::acosh_(*this);
+    }
+
     if (!std::is_arithmetic_v<value_type>)
-        throw type_error("Type must be arithmetic");
+    {
+        throw error::type_error("Type must be arithmetic");
+    }
 
     for (auto& elem : data_)
     {
         if (elem < 1.0)
+        {
             throw std::domain_error("Input data is out of domain of acosh()");
+        }
 
         elem = std::acosh(elem);
     }
@@ -133,6 +150,11 @@ inline const tensor<_Tp>& tensor<_Tp>::acosh_() const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::acosh() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.acosh_();
     return ret;
