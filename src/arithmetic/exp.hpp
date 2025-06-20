b6cdb48102dda6ce22222ed7672dec4d38b08761
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-20 12:29:26 +0100

    stuff

--- src/arithmetic/exp.hpp
+++ src/arithmetic/exp.hpp
@@ -2,26 +2,22 @@
 
 #include "tensorbase.hpp"
 
-template<class _Tp>
-tensor<_Tp>& tensor<_Tp>::exp_() {
-    if (!std::is_arithmetic_v<value_type>)
-    {
-        throw type_error("Type must be arithmetic");
-    }
-
-    for (auto& elem : data_)
-    {
-        elem = std::exp(elem);
-    }
-
-    return *this;
-}
 
 template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::exp_() const {
+inline tensor<_Tp>& tensor<_Tp>::exp_() {
+    if (empty())
+    {
+        return *this;
+    }
+
+    if (internal::types::using_neon())
+    {
+        return internal::neon::exp_(*this);
+    }
+
     if (!std::is_arithmetic_v<value_type>)
     {
-        throw type_error("Type must be arithmetic");
+        throw error::type_error("Type must be arithmetic");
     }
 
     for (auto& elem : data_)
@@ -34,6 +30,11 @@ inline const tensor<_Tp>& tensor<_Tp>::exp_() const {
 
 template<class _Tp>
 inline tensor<_Tp> tensor<_Tp>::exp() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.exp_();
     return ret;
