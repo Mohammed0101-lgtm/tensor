24b069f7232e5ee1836a91627457636165977a98
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-06-20 12:31:12 +0100

    stuff_2

--- src/arithmetic/abs.hpp
+++ src/arithmetic/abs.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "../types.hpp"
 #include "tensorbase.hpp"
 
 template<class _Tp>
 tensor<_Tp> tensor<_Tp>::abs() const {
+    if (empty())
+    {
+        return self({0});
+    }
+
     self ret = clone();
     ret.abs_();
     return ret;
@@ -11,34 +17,25 @@ tensor<_Tp> tensor<_Tp>::abs() const {
 
 template<class _Tp>
 tensor<_Tp>& tensor<_Tp>::abs_() {
-#if defined(__ARM_NEON)
-    return neon_abs_();
-#endif
+    if (internal::types::using_neon())
+    {
+        return internal::neon::abs_(*this);
+    }
+
     if (!std::is_arithmetic_v<value_type>)
-        throw type_error("abs_() is only available for arithmetic types.");
+    {
+        throw error::type_error("abs_() is only available for arithmetic types.");
+    }
 
     if (std::is_unsigned_v<value_type>)
+    {
         return *this;
+    }
 
     for (auto& elem : data_)
+    {
         elem = std::abs(elem);
-
-    return *this;
-}
-
-template<class _Tp>
-inline const tensor<_Tp>& tensor<_Tp>::abs_() const {
-#if defined(__ARM_NEON)
-    return neon_abs_();
-#endif
-    if (!std::is_arithmetic_v<value_type>)
-        throw type_error("abs_() is only available for arithmetic types.");
-
-    if (std::is_unsigned_v<value_type>)
-        return *this;
-
-    for (auto& elem : data_)
-        elem = std::abs(elem);
+    }
 
     return *this;
 }
