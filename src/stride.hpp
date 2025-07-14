6d49e09c355bbcad3176f5192e3c90e69e846233
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-07-14 01:46:56 +0100

    stuff

--- src/stride.hpp
+++ src/stride.hpp
@@ -8,65 +8,65 @@ namespace shape {
 
 struct Strides
 {
-    using index        = uint64_t;
-    using strides_type = std::vector<index>;
+  using index        = uint64_t;
+  using strides_type = std::vector<index>;
 
-    strides_type value_;
+  strides_type value_;
 
-    Strides() { value_ = strides_type(); }
+  Strides() { value_ = strides_type(); }
 
-    Strides(const std::vector<index>& shape) {
-        if (shape.empty())
-        {
-            throw error::shape_error("Shape must be initialized before computing strides");
-        }
-
-        value_     = std::vector<index>(shape.size(), 1);
-        int stride = 1;
-
-        for (int i = static_cast<int>(shape.size() - 1); i >= 0; i--)
-        {
-            value_[i] = stride;
-            stride *= shape[i];
-        }
+  Strides(const std::vector<index>& shape) {
+    if (shape.empty())
+    {
+      throw error::shape_error("Shape must be initialized before computing strides");
     }
 
-    Strides(const Strides& other) noexcept :
-        value_(other.value_) {}
+    value_     = std::vector<index>(shape.size(), 1);
+    int stride = 1;
 
-    Strides& operator=(const Strides& other) noexcept {
-        if (this != &other)
-        {
-            value_ = other.value_;
-        }
+    for (int i = static_cast<int>(shape.size() - 1); i >= 0; i--)
+    {
+      value_[i] = stride;
+      stride *= shape[i];
+    }
+  }
 
-        return *this;
+  Strides(const Strides& other) noexcept :
+      value_(other.value_) {}
+
+  Strides& operator=(const Strides& other) noexcept {
+    if (this != &other)
+    {
+      value_ = other.value_;
     }
 
-    bool operator==(const Strides& other) const { return this->value_ == other.value_; }
+    return *this;
+  }
 
-    index operator[](const index at) const { return value_[at]; }
+  bool operator==(const Strides& other) const { return this->value_ == other.value_; }
 
-    index& operator[](const index at) { return value_[at]; }
+  index operator[](const index at) const { return value_[at]; }
 
-    strides_type get() const { return value_; }
+  index& operator[](const index at) { return value_[at]; }
 
-    void compute_strides(const std::vector<index>& shape_) noexcept {
-        if (shape_.empty())
-        {
-            value_ = strides_type();
-            return;
-        }
+  strides_type get() const { return value_; }
 
-        value_ = strides_type(shape_.size(), 1);
-        int st = 1, i = static_cast<int>(shape_.size() - 1);
-
-        for (; i >= 0; i--)
-        {
-            value_[i] = st;
-            st *= shape_[i];
-        }
+  void compute_strides(const std::vector<index>& shape_) noexcept {
+    if (shape_.empty())
+    {
+      value_ = strides_type();
+      return;
     }
+
+    value_ = strides_type(shape_.size(), 1);
+    int st = 1, i = static_cast<int>(shape_.size() - 1);
+
+    for (; i >= 0; i--)
+    {
+      value_[i] = st;
+      st *= shape_[i];
+    }
+  }
 };
 
 }  // namespace shape
