0960cd312910f83bfffb85967f627ea91e2e9831
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-07-24 01:13:19 +0100

    stuff

--- tests/test_types.cpp
+++ tests/test_types.cpp
@@ -1,23 +1,59 @@
 #include "tensor.hpp"
 #include <gtest/gtest.h>
-#include <iostream>
 
 
-TEST(TensorTest, DataTypeConversionTest) {
-  tensor<int>       t({3}, {1, 2, 3});
-  tensor<int32_t>   t_int       = t.int32_();
-  tensor<int16_t>   t_short     = t.int16_();
-  tensor<uint32_t>  t_uint      = t.uint32_();
-  tensor<uint64_t>  t_ulong     = t.uint64_();
-  tensor<float32_t> t_float     = t.float32_();
-  tensor<float64_t> t_double    = t.float64_();
-  tensor<int64_t>   t_long_long = t.int64_();
+TEST(TypesTest, IntToInt32Conversion) {
+  tensor<int>          t({3}, {1, 2, 3});
+  tensor<int32_t>      result   = t.int32_();
+  std::vector<int32_t> expected = {1, 2, 3};
 
-  EXPECT_EQ(t_int.storage(), (std::vector<int32_t>{1, 2, 3}));
-  EXPECT_EQ(t_uint.storage(), (std::vector<uint32_t>{1, 2, 3}));
-  EXPECT_EQ(t_ulong.storage(), (std::vector<uint64_t>{1, 2, 3}));
-  EXPECT_EQ(t_float.storage(), (std::vector<float32_t>{1.0, 2.0, 3.0}));
-  EXPECT_EQ(t_double.storage(), (std::vector<float64_t>{1.0, 2.0, 3.0}));
-  EXPECT_EQ(t_short.storage(), std::vector<int16_t>({1, 2, 3}));
-  EXPECT_EQ(t_long_long.storage(), std::vector<int64_t>({1, 2, 3}));
+  EXPECT_EQ(result.storage(), expected);
+}
+
+TEST(TypesTest, IntToInt16Conversion) {
+  tensor<int>          t({3}, {1, 2, 3});
+  tensor<int16_t>      result   = t.int16_();
+  std::vector<int16_t> expected = {1, 2, 3};
+
+  EXPECT_EQ(result.storage(), expected);
+}
+
+TEST(TypesTest, IntToUInt32Conversion) {
+  tensor<int>           t({3}, {1, 2, 3});
+  tensor<uint32_t>      result   = t.uint32_();
+  std::vector<uint32_t> expected = {1, 2, 3};
+
+  EXPECT_EQ(result.storage(), expected);
+}
+
+TEST(TypesTest, IntToUInt64Conversion) {
+  tensor<int>           t({3}, {1, 2, 3});
+  tensor<uint64_t>      result   = t.uint64_();
+  std::vector<uint64_t> expected = {1, 2, 3};
+
+  EXPECT_EQ(result.storage(), expected);
+}
+
+TEST(TypesTest, IntToFloat32Conversion) {
+  tensor<int>            t({3}, {1, 2, 3});
+  tensor<float32_t>      result   = t.float32_();
+  std::vector<float32_t> expected = {1.0f, 2.0f, 3.0f};
+
+  EXPECT_EQ(result.storage(), expected);
+}
+
+TEST(TypesTest, IntToFloat64Conversion) {
+  tensor<int>            t({3}, {1, 2, 3});
+  tensor<float64_t>      result   = t.float64_();
+  std::vector<float64_t> expected = {1.0, 2.0, 3.0};
+
+  EXPECT_EQ(result.storage(), expected);
+}
+
+TEST(TypesTest, IntToInt64Conversion) {
+  tensor<int>          t({3}, {1, 2, 3});
+  tensor<int64_t>      result   = t.int64_();
+  std::vector<int64_t> expected = {1, 2, 3};
+
+  EXPECT_EQ(result.storage(), expected);
 }
