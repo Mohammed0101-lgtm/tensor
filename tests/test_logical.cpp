ca5a7f2cea33340c822d1057d6af247c970d2a5a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-07-28 17:43:25 +0100

    stuff

--- tests/test_logical.cpp
+++ tests/test_logical.cpp
@@ -2,17 +2,193 @@
 #include <gtest/gtest.h>
 
 
-TEST(LogicalTest, LogicalTest) {
-  tensor<int> t({2, 2}, {1, 2, 3, 4});
+template<typename T>
+class TensorLogicTest: public ::testing::Test
+{
+ protected:
+  using tensor_t = tensor<T>;
 
-  tensor<bool> bool_       = t.bool_();
-  tensor<bool> logical_not = bool_.logical_not();
-  tensor<bool> logical_or  = bool_.logical_or(true);
-  tensor<bool> logical_and = bool_.logical_and(false);
-  tensor<bool> logical_xor = bool_.logical_xor(false);
+  tensor_t make_tensor(std::vector<T> data, std::vector<uint64_t> shape = {}) {
+    if (shape.empty())
+    {
+      shape = {data.size()};
+    }
 
-  EXPECT_EQ(logical_not, tensor<bool>({2, 2}, {false, false, false, false}));
-  EXPECT_EQ(logical_or, tensor<bool>({2, 2}, {true, true, true, true}));
-  EXPECT_EQ(logical_xor, tensor<bool>({2, 2}, {false, false, false, false}));
-  EXPECT_EQ(logical_and, tensor<bool>({2, 2}, {false, false, false, false}));
+    return tensor_t(shape::Shape(shape), data);
+  }
+};
+
+
+using MyTypes = ::testing::Types<bool, int, float>;
+TYPED_TEST_SUITE(TensorLogicTest, MyTypes);
+
+
+TYPED_TEST(TensorLogicTest, LogicalNotBasic) {
+  auto t      = this->make_tensor({1, 0, 1, 0});
+  auto result = t.logical_not();
+  EXPECT_EQ(result[0], false);
+  EXPECT_EQ(result[1], true);
+  EXPECT_EQ(result[2], false);
+  EXPECT_EQ(result[3], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalNotAllZeros) {
+  auto t      = this->make_tensor({0, 0, 0});
+  auto result = t.logical_not();
+  auto data   = result.storage_();
+  for (auto v : data)
+  {
+    EXPECT_TRUE(v);
+  }
+}
+
+TYPED_TEST(TensorLogicTest, LogicalNotAllOnes) {
+  auto t      = this->make_tensor({1, 1, 1});
+  auto result = t.logical_not();
+  auto data   = result.storage_();
+  for (auto v : data)
+  {
+    EXPECT_FALSE(v);
+  }
+}
+
+TYPED_TEST(TensorLogicTest, LogicalNotInplace) {
+  auto t = this->make_tensor({1, 0});
+  t.logical_not_();
+  
+  EXPECT_EQ(t[0], false);
+  EXPECT_EQ(t[1], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalOrScalar) {
+  auto t      = this->make_tensor({0, 1, 0});
+  auto result = t.logical_or(1);
+  auto data   = result.storage_();
+  
+  for (auto v : data)
+  {
+    EXPECT_TRUE(v);
+  }
+}
+
+TYPED_TEST(TensorLogicTest, LogicalOrScalarFalse) {
+  auto t      = this->make_tensor({0, 0, 0}); 
+  auto result = t.logical_or(0);
+  auto data   = result.storage_();
+
+  for (auto v : data)
+  {
+    EXPECT_FALSE(v);
+  }
+}
+
+TYPED_TEST(TensorLogicTest, LogicalOrScalarTrue) {
+  auto t      = this->make_tensor({0, 1});
+  auto result = t.logical_or(1);
+ 
+  EXPECT_EQ(result[0], true);
+  EXPECT_EQ(result[1], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalOrTensorSameShape) {
+  auto a      = this->make_tensor({1, 0, 0});
+  auto b      = this->make_tensor({0, 0, 1});
+  auto result = a.logical_or(b);
+ 
+  EXPECT_EQ(result[0], true);
+  EXPECT_EQ(result[1], false);
+  EXPECT_EQ(result[2], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalOrInplaceScalar) {
+  auto t = this->make_tensor({0, 1});
+  t.logical_or_(0);
+  EXPECT_EQ(t[0], false);
+  EXPECT_EQ(t[1], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalOrInplaceTensor) {
+  auto a = this->make_tensor({0, 1});
+  auto b = this->make_tensor({1, 0});
+  a.logical_or_(b);
+ 
+  EXPECT_EQ(a[0], true);
+  EXPECT_EQ(a[1], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalXorScalar) {
+  auto t      = this->make_tensor({1, 0, 1});
+  auto result = t.logical_xor(1);
+  EXPECT_EQ(result[0], false);
+  EXPECT_EQ(result[1], true);
+  EXPECT_EQ(result[2], false);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalXorTensor) {
+  auto a      = this->make_tensor({1, 0, 1});
+  auto b      = this->make_tensor({1, 1, 0});
+  auto result = a.logical_xor(b);
+  EXPECT_EQ(result[0], false);
+  EXPECT_EQ(result[1], true);
+  EXPECT_EQ(result[2], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalXorInplaceScalar) {
+  auto t = this->make_tensor({1, 1});
+  t.logical_xor_(1);
+  auto data = t.storage_();
+  for (auto v : data)
+  {
+    EXPECT_FALSE(v);
+  }
+}
+
+TYPED_TEST(TensorLogicTest, LogicalXorInplaceTensor) {
+  auto a = this->make_tensor({1, 0});
+  auto b = this->make_tensor({0, 0});
+  a.logical_xor_(b);
+  EXPECT_EQ(a[0], true);
+  EXPECT_EQ(a[1], false);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalAndScalarTrue) {
+  auto t      = this->make_tensor({1, 0, 1});
+  auto result = t.logical_and(1);
+  EXPECT_EQ(result[0], true);
+  EXPECT_EQ(result[1], false);
+  EXPECT_EQ(result[2], true);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalAndScalarFalse) {
+  auto t      = this->make_tensor({1, 1, 1});
+  auto result = t.logical_and(0);
+  auto data   = result.storage_();
+  for (auto v : data)
+  {
+    EXPECT_FALSE(v);
+  }
+}
+
+TYPED_TEST(TensorLogicTest, LogicalAndTensor) {
+  auto a      = this->make_tensor({1, 0, 1});
+  auto b      = this->make_tensor({1, 1, 0});
+  auto result = a.logical_and(b);
+  EXPECT_EQ(result[0], true);
+  EXPECT_EQ(result[1], false);
+  EXPECT_EQ(result[2], false);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalAndInplaceScalar) {
+  auto t = this->make_tensor({1, 0});
+  t.logical_and_(1);
+  EXPECT_EQ(t[0], true);
+  EXPECT_EQ(t[1], false);
+}
+
+TYPED_TEST(TensorLogicTest, LogicalAndInplaceTensor) {
+  auto a = this->make_tensor({1, 0});
+  auto b = this->make_tensor({1, 1});
+  a.logical_and_(b);
+  EXPECT_EQ(a[0], true);
+  EXPECT_EQ(a[1], false);
 }
