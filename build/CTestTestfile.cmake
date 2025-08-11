dc2d85b0861a629c9cdefa2fc87097e09c42888f
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-08-11 06:08:23 +0100

    stuff

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mohamedrabbit/tensor
-# Build directory: /Users/mohamedrabbit/tensor/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTestsInMain "/Users/mohamedrabbit/tensor/build/tensor_test")
-set_tests_properties(AllTestsInMain PROPERTIES  _BACKTRACE_TRIPLES "/Users/mohamedrabbit/tensor/CMakeLists.txt;91;add_test;/Users/mohamedrabbit/tensor/CMakeLists.txt;0;")
